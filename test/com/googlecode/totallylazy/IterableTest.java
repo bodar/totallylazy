21c7441c8a07a4d8ec813f4b5315ad509fd640bf
Daniel Worthington-Bodart <dan@bodar.com>
2010-09-20 21:46:49 +0100

    dan: readded missing tests

--- test/com/googlecode/totallylazy/IterableTest.java
+++ test/com/googlecode/totallylazy/IterableTest.java
@@ -14,10 +14,20 @@ import static org.junit.matchers.JUnitMatchers.hasItems;
 
 public class IterableTest {
     @Test
+    public void supportsTail() throws Exception {
+        assertThat(list(1, 2, 3).tail(), hasItems(2, 3));
+    }
+
+    @Test
+    public void supportsHead() throws Exception {
+        assertThat(list(1, 2).head(), is(1));
+    }
+
+    @Test
     public void supportsForEach() throws Exception {
         final int[] sum = {0};
         list(1, 2).foreach(new Runnable1<Integer>() {
-            public void run(Integer value){
+            public void run(Integer value) {
                 sum[0] += value;
             }
         });
@@ -26,7 +36,7 @@ public class IterableTest {
 
     @Test
     public void supportsMap() throws Exception {
-        Iterable<String> strings = list(1,2).map(asString(Integer.class));
+        Iterable<String> strings = list(1, 2).map(asString(Integer.class));
         assertThat(strings, hasItems("1", "2"));
     }
 
@@ -39,11 +49,11 @@ public class IterableTest {
 
     @Test
     public void supportsFilter() throws Exception {
-        Iterable<Integer> result = list(1,2,3,4).filter(even());
-        assertThat(result, hasItems(2,4));
+        Iterable<Integer> result = list(1, 2, 3, 4).filter(even());
+        assertThat(result, hasItems(2, 4));
     }
 
-       @Test
+    @Test
     public void filterIsLazy() throws Exception {
         Iterable<Integer> result = list(returns(1), returns(2), callThrows(new Exception(), Integer.class)).
                 map(invokeCall(Integer.class)).
@@ -53,11 +63,11 @@ public class IterableTest {
 
     @Test
     public void supportsFlatMap() throws Exception {
-        Iterable<Integer> result = list(1,2,3).flatMap(new Callable1<Integer, Iterable<Integer>>() {
+        Iterable<Integer> result = list(1, 2, 3).flatMap(new Callable1<Integer, Iterable<Integer>>() {
             public Iterable<Integer> call(Integer value) throws Exception {
                 return list(value, value * 3);
             }
         });
-        assertThat(result, hasItems(1,2,3,6,9));
+        assertThat(result, hasItems(1, 2, 3, 6, 9));
     }
 }
