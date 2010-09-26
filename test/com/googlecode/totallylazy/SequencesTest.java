37309abc7902fbe0d7e03509be10088816f0c632
Daniel Worthington-Bodart <dan@bodar.com>
2010-09-26 13:49:01 +0100

    Added startsWith matcher for infinite collections

--- test/com/googlecode/totallylazy/SequencesTest.java
+++ test/com/googlecode/totallylazy/SequencesTest.java
@@ -2,28 +2,25 @@ package com.googlecode.totallylazy;
 
 import org.junit.Test;
 
-import static com.googlecode.totallylazy.Callables.add;
 import static com.googlecode.totallylazy.Callables.increment;
-import static com.googlecode.totallylazy.HasExactlyMatcher.hasExactly;
+import static com.googlecode.totallylazy.IterableMatcher.hasExactly;
+import static com.googlecode.totallylazy.IterableMatcher.startsWith;
 import static com.googlecode.totallylazy.Predicates.even;
 import static com.googlecode.totallylazy.Predicates.odd;
 import static com.googlecode.totallylazy.Sequences.*;
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.hasItem;
-import static org.junit.matchers.JUnitMatchers.hasItems;
 
 public class SequencesTest {
     @Test
     public void supportsPrimes() throws Exception {
         Sequence<Integer> primes = primes();
-        assertThat(primes, hasItems(2,3,5,7,11,13,17,19,23,29));
+        assertThat(primes, startsWith(2,3,5,7,11,13,17,19,23,29));
     }
 
     @Test
     public void supportsFibonacci() throws Exception {
-        assertThat(fibonacci(), hasItems(0, 1, 1, 2, 3, 5));
+        assertThat(fibonacci(), startsWith(0, 1, 1, 2, 3, 5));
     }
 
     @Test
@@ -42,13 +39,13 @@ public class SequencesTest {
     @Test
     public void supportsIterate() throws Exception {
         Sequence<Integer> numbers = iterate(increment(), 1);
-        assertThat(numbers, hasItems(1, 2, 3, 4, 5));
+        assertThat(numbers, startsWith(1, 2, 3, 4, 5));
     }
 
     @Test
     public void canCombineIterateWithOtherOperations() throws Exception {
         final Sequence<Integer> numbers = iterate(increment(), 1);
-        assertThat(numbers.filter(even()), hasItems(2, 4, 6));
-        assertThat(numbers.filter(odd()), hasItems(1, 3, 5, 7, 9));
+        assertThat(numbers.filter(even()), startsWith(2, 4, 6));
+        assertThat(numbers.filter(odd()), startsWith(1, 3, 5, 7, 9));
     }
 }
