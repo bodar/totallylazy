219c0f52bf14b09fbdf26ead8db3540e3ea6e215
Daniel Worthington-Bodart <dan@bodar.com>
2010-09-28 21:30:40 +0100

    Made memorise thread safe

--- src/com/googlecode/totallylazy/MemoriseSequence.java
+++ src/com/googlecode/totallylazy/MemoriseSequence.java
@@ -3,12 +3,15 @@ package com.googlecode.totallylazy;
 import com.googlecode.totallylazy.iterators.MemoriseIterator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
+import static java.util.Collections.synchronizedList;
+
 public class MemoriseSequence<T> extends Sequence<T> {
     private final Iterable<T> iterable;
-    private final List<T> memory = new ArrayList<T>();
+    private final List<T> memory = synchronizedList(new ArrayList<T>());
 
     public MemoriseSequence(Iterable<T> iterable) {
         this.iterable = iterable;
