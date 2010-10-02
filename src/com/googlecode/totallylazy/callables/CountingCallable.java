314aa92c6ef4e20115bdca6a05120aff274e15df
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-02 22:27:02 +0100

    Added simple Lazy, Sleepy and Counting Callables

---
+++ src/com/googlecode/totallylazy/callables/CountingCallable.java
@@ -0,0 +1,22 @@
+package com.googlecode.totallylazy.callables;
+
+import java.util.concurrent.Callable;
+
+public class CountingCallable implements Callable<Integer> {
+    private int count = 0;
+
+    private CountingCallable() {
+    }
+
+    public Integer call() throws Exception {
+        return count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static CountingCallable counting() {
+        return new CountingCallable();
+    }
+}
