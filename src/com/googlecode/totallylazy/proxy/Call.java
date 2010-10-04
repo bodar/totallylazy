d10d45ca93b6567a00619312072f0cf875af6a27
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-04 11:51:01 +0100

    Minor clean around proxies

--- src/com/googlecode/totallylazy/proxy/Call.java
+++ src/com/googlecode/totallylazy/proxy/Call.java
@@ -10,13 +10,15 @@ import static com.googlecode.totallylazy.proxy.Proxy.createProxy;
 public class Call implements InvocationHandler{
     public static ThreadLocalInvocation invocation = new ThreadLocalInvocation();
 
-    public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-        invocation.set(new Invocation(proxy, method, arguments));
-        return null;
-    }
+    private Call() {}
 
     public static <T> T on(Class<T> aCLass){
          return createProxy(aCLass, new Call());
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+        invocation.set(new Invocation(proxy, method, arguments));
+        return null;
     }
 
     public static <T, S> Callable1<T,S> method(S value){
