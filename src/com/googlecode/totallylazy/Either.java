bb890c6f249aa0b714b4c6805e89fa3a15c7a4d1
Daniel Worthington-Bodart <dan@bodar.com>
2010-11-04 11:33:25 +0000

    Added fold, toString, equals and hashCode to Either

--- src/com/googlecode/totallylazy/Either.java
+++ src/com/googlecode/totallylazy/Either.java
@@ -18,4 +18,8 @@ public abstract class Either<L, R> {
     public L left(){
         throw new NoSuchElementException();
     }
+
+    public <S> S fold(final S seed, final Callable2<? super S, L, S> left, final Callable2<? super S, R, S> right) {
+        return isLeft() ? Callers.call(left, seed, left()) : Callers.call(right, seed, right());
+    }
 }
