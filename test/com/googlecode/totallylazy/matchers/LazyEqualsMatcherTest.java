5d9c3b8f9ef01917a788cfa5ef49b42da5b78212
Daniel Worthington-Bodart <dan@bodar.com>
2011-01-21 12:01:47 +0000

    Improve LazyEqualsMatcher output

--- test/com/googlecode/totallylazy/matchers/LazyEqualsMatcherTest.java
+++ test/com/googlecode/totallylazy/matchers/LazyEqualsMatcherTest.java
@@ -1,6 +1,7 @@
 package com.googlecode.totallylazy.matchers;
 
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.hamcrest.StringDescription;
 import org.junit.Test;
 
@@ -11,25 +12,35 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNot.not;
 
 public class LazyEqualsMatcherTest {
-    private static final String DESCRIPTION_TEXT = "description";
+    private static final String SIMPLE_DESCRIPTION_TEXT = "description";
     private static final String EXPECTED = "myObject";
     private static final String ACTUAL = EXPECTED;
     private static final String DIFFERENT_ACTUAL = "different";
+    private static final String FULL_DESCRIPTION_TEXT = SIMPLE_DESCRIPTION_TEXT + " [expected: " + EXPECTED + ", actual: " + DIFFERENT_ACTUAL + "]";
 
     @Test
     public void shouldBeEqual() {
-        assertThat(ACTUAL, lazyEqualTo(DESCRIPTION_TEXT, returns(EXPECTED)));
+        assertThat(ACTUAL, lazyEqualTo(SIMPLE_DESCRIPTION_TEXT, returns(EXPECTED)));
     }
 
     @Test
     public void shouldNotBeEqual() {
-        assertThat(DIFFERENT_ACTUAL, not(lazyEqualTo(DESCRIPTION_TEXT, returns(EXPECTED))));
+        assertThat(DIFFERENT_ACTUAL, not(lazyEqualTo(SIMPLE_DESCRIPTION_TEXT, returns(EXPECTED))));
     }
 
     @Test
     public void shouldDescribeEquality() {
         Description description = new StringDescription();
-        lazyEqualTo(DESCRIPTION_TEXT, returns(EXPECTED)).describeTo(description);
-        assertThat(description.toString(), is(DESCRIPTION_TEXT));
+        lazyEqualTo(SIMPLE_DESCRIPTION_TEXT, returns(EXPECTED)).describeTo(description);
+        assertThat(description.toString(), is(SIMPLE_DESCRIPTION_TEXT));
+    }
+
+    @Test
+    public void shouldDescribeEqualityWithExpectedAndActual() {
+        Description description = new StringDescription();
+        Matcher<String> matcher = lazyEqualTo(SIMPLE_DESCRIPTION_TEXT, returns(EXPECTED));
+        matcher.matches(DIFFERENT_ACTUAL);
+        matcher.describeTo(description);
+        assertThat(description.toString(), is(FULL_DESCRIPTION_TEXT));
     }
 }
