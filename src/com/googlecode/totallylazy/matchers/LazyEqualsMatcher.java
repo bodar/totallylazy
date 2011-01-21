a66f305fa18455c7c2f59d4a332657a1183bf39e
Daniel Wothington-Bodart <dan@bodar.com>
2011-01-21 12:01:47 +0000

    Improve LazyEqualsMatcher output

--- src/com/googlecode/totallylazy/matchers/LazyEqualsMatcher.java
+++ src/com/googlecode/totallylazy/matchers/LazyEqualsMatcher.java
@@ -10,6 +10,8 @@ import static com.googlecode.totallylazy.Callers.call;
 public class LazyEqualsMatcher<T> extends TypeSafeMatcher<T> {
     private final Callable<T> expectedLoader;
     private final String descriptionText;
+    private T actual;
+    private T expected;
 
     private LazyEqualsMatcher(String descriptionText, Callable<T> expectedLoader) {
         this.expectedLoader = expectedLoader;
@@ -17,12 +19,19 @@ public class LazyEqualsMatcher<T> extends TypeSafeMatcher<T> {
     }
 
     @Override
-    protected boolean matchesSafely(T actual) {
-        return call(expectedLoader).equals(actual);
+    protected boolean matchesSafely(T actualValue) {
+        actual = actualValue;
+        expected = call(expectedLoader);
+        
+        return expected.equals(actual);
     }
 
     public void describeTo(Description description) {
-        description.appendText(descriptionText);
+        description.appendText(createDescriptionText());
+    }
+
+    private String createDescriptionText() {
+        return expected != null ? String.format(descriptionText + " [expected: %s, actual: %s]", expected, actual) : descriptionText;
     }
 
     public static <E> LazyEqualsMatcher<E> lazyEqualTo(String descriptionText, Callable<E> expectedLoader) {
