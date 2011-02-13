85fcb96e191ab7660e2ef81060751f37ad1afc75
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-13 19:34:05 +0000

    Added support for temporary files, directories and getting sequences of files from a directory

--- src/com/googlecode/totallylazy/Files.java
+++ src/com/googlecode/totallylazy/Files.java
@@ -1,36 +1,57 @@
 package com.googlecode.totallylazy;
 
 import java.io.File;
+import java.io.IOException;
 
 import static java.lang.System.getProperty;
 import static java.util.UUID.randomUUID;
 
 public class Files {
-    public static Callable1<File, String> getName() {
-         return new Callable1<File, String>() {
-             public String call(File file) throws Exception {
-                 return file.getName();
-             }
-         };
-     }
+    public static final File TEMP_DIR = new File(getProperty("java.io.tmpdir"));
 
-     public static Callable1<? super File, File> getParentFile() {
-         return new Callable1<File, File>() {
-             public File call(File file) throws Exception {
-                 return file.getParentFile();
-             }
-         };
-     }
-
-    public static File temporaryFile(){
-        File file = new File(getProperty("java.io.tmpdir"), randomUUID().toString());
-        file.deleteOnExit();
-        return file;
+    public static Callable1<File, String> name() {
+        return new Callable1<File, String>() {
+            public String call(File file) throws Exception {
+                return file.getName();
+            }
+        };
     }
 
-    public static File temporaryDirectory(){
-        File file = temporaryFile();
-        file.mkdirs();
-        return file;
+    public static Callable1<? super File, File> parent() {
+        return new Callable1<File, File>() {
+            public File call(File file) throws Exception {
+                return file.getParentFile();
+            }
+        };
+    }
+
+    public static File temporaryDirectory() {
+        File directory = new File(TEMP_DIR, randomFilename());
+        directory.deleteOnExit();
+        directory.mkdirs();
+        return directory;
+    }
+
+    public static File temporaryFile() {
+        return temporaryFile(TEMP_DIR);
+    }
+
+    public static File temporaryFile(File directory) {
+        try {
+            File file = new File(directory, randomFilename());
+            file.deleteOnExit();
+            file.createNewFile();
+            return file;
+        } catch (IOException e) {
+            throw new LazyException(e);
+        }
+    }
+
+    public static String randomFilename() {
+        return randomUUID().toString();
+    }
+
+    public static Sequence<File> files(File directory) {
+        return Sequences.sequence(directory.listFiles());
     }
 }
