9acc17c45609f223bcacd5725116cbccd8c9f066
Daniel Worthington-Bodart <dan@bodar.com>
2011-02-14 14:53:32 +0000

    Moved some behaviour from ShavenMaven

--- src/com/googlecode/totallylazy/Files.java
+++ src/com/googlecode/totallylazy/Files.java
@@ -1,7 +1,9 @@
 package com.googlecode.totallylazy;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import static java.lang.System.getProperty;
 import static java.util.UUID.randomUUID;
@@ -54,4 +56,22 @@ public class Files {
     public static Sequence<File> files(File directory) {
         return Sequences.sequence(directory.listFiles());
     }
+
+    public static File write(byte[] bytes, File file) throws IOException {
+        OutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(bytes);
+        outputStream.close();
+        return file;
+    }
+
+    public static Callable1<? super File, Boolean> delete() {
+        return new Callable1<File, Boolean>() {
+            public Boolean call(File file) throws Exception {
+                return file.delete();
+            }
+        };
+    }
+
+
+
 }
