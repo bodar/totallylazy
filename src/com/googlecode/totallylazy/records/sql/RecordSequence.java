bfb0bab287f141d652f8ddf87746ae8b6fe9f101
Daniel Worthington-Bodart <dan@bodar.com>
2011-03-22 16:17:29 +0000

    Replaced Logging with PrintStream so that it can be controlled like any other dependency

--- src/com/googlecode/totallylazy/records/sql/RecordSequence.java
+++ src/com/googlecode/totallylazy/records/sql/RecordSequence.java
@@ -9,20 +9,23 @@ import com.googlecode.totallylazy.records.Queryable;
 import com.googlecode.totallylazy.records.Record;
 import com.googlecode.totallylazy.records.SelectCallable;
 
+import java.io.PrintStream;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.logging.Level;
 
 import static com.googlecode.totallylazy.Callables.ascending;
+import static java.lang.String.format;
 
 public class RecordSequence extends Sequence<Record> implements QuerySequence {
     private final Queryable queryable;
     private final Query query;
+    private final PrintStream logger;
 
-    public RecordSequence(final Queryable queryable, final Query query) {
+    public RecordSequence(final Queryable queryable, final Query query, final PrintStream logger) {
         this.queryable = queryable;
         this.query = query;
+        this.logger = logger;
     }
 
     public Iterator<Record> iterator() {
@@ -40,17 +43,18 @@ public class RecordSequence extends Sequence<Record> implements QuerySequence {
             return new SingleValueSequence<S>(queryable, query.select((Keyword) callable), callable);
         }
         if (callable instanceof SelectCallable) {
-            return (Sequence<S>) new RecordSequence(queryable, query.select(((SelectCallable) callable).keywords()));
+            return (Sequence<S>) new RecordSequence(queryable, query.select(((SelectCallable) callable).keywords()), logger);
         }
-        Sql.LOGGER.log(Level.FINE, String.format("Warning: unsupported callables %s dropping down to client side sequence functionality", callable));
+        logger.println(format("Warning: unsupported callables %s dropping down to client side sequence functionality", callable));
         return super.map(callable);
     }
 
     @Override
     public Sequence<Record> filter(Predicate<? super Record> predicate) {
         if (query.sql().isSupported(predicate)) {
-            return new RecordSequence(queryable, query.where(predicate));
+            return new RecordSequence(queryable, query.where(predicate), logger);
         }
+        logger.println(format("Warning: Unsupported predicate %s dropping down to client side sequence functionality", predicate));
         return super.filter(predicate);
     }
 
@@ -62,8 +66,9 @@ public class RecordSequence extends Sequence<Record> implements QuerySequence {
     @Override
     public Sequence<Record> sortBy(Comparator<? super Record> comparator) {
         if (query.sql().isSupported(comparator)) {
-            return new RecordSequence(queryable, query.orderBy(comparator));
+            return new RecordSequence(queryable, query.orderBy(comparator), logger);
         }
+        logger.println(format("Warning: unsupported comparator %s dropping down to client side sequence functionality", comparator));
         return super.sortBy(comparator);
     }
 
