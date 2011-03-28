efd5fd34ebbeaca36e494aabf41d7402b306f6eb
Daniel Worthington-Bodart <dan@bodar.com>
2011-03-28 22:12:55 +0100

    Cleaned up lines

--- test/com/googlecode/totallylazy/StringsTest.java
+++ test/com/googlecode/totallylazy/StringsTest.java
@@ -2,8 +2,13 @@ package com.googlecode.totallylazy;
 
 import org.junit.Test;
 
+import java.io.File;
+
+import static com.googlecode.totallylazy.Files.temporaryFile;
+import static com.googlecode.totallylazy.Files.write;
 import static com.googlecode.totallylazy.Sequences.repeat;
 import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.Strings.lines;
 import static com.googlecode.totallylazy.Strings.toLowerCase;
 import static com.googlecode.totallylazy.Strings.toUpperCase;
 import static com.googlecode.totallylazy.matchers.IterableMatcher.hasExactly;
@@ -12,20 +17,27 @@ import static org.hamcrest.Matchers.is;
 
 public class StringsTest {
     @Test
+    public void parsesLines() throws Exception {
+        File file = temporaryFile();
+        write("1\r\n2".getBytes("UTF-8"), file);
+        Sequence<String> lines = lines(file);
+        assertThat(lines, hasExactly("1", "2"));
+    }
+    @Test
     public void canMapToStringFunctions() throws Exception {
-       assertThat(sequence("Dan").map(toLowerCase()), hasExactly("dan"));
-       assertThat(sequence("Dan").map(toUpperCase()), hasExactly("DAN"));
+        assertThat(sequence("Dan").map(toLowerCase()), hasExactly("dan"));
+        assertThat(sequence("Dan").map(toUpperCase()), hasExactly("DAN"));
     }
 
     @Test
     public void canEscapeXml() throws Exception {
-        assertThat(Strings.escapeXml("& < > ' " + new Character((char)0x80)), is("&amp; &lt; &gt; &apos; &#128;"));
+        assertThat(Strings.escapeXml("& < > ' " + new Character((char) 0x80)), is("&amp; &lt; &gt; &apos; &#128;"));
     }
 
-     @Test
+    @Test
     public void doesNotTruncateString() throws Exception {
         String testString = longStringWithoutEncodedChars();
-         assertThat(Strings.escapeXml(testString), is(testString));
+        assertThat(Strings.escapeXml(testString), is(testString));
     }
 
     private String longStringWithoutEncodedChars() {
