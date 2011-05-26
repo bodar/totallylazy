bea1c3b111def742be4b351345f03f6bbc3952b4
Daniel Worthington-Bodart <dan@bodar.com>
2011-05-26 13:54:44 +0100

    Extracted method onto correct class (added missing method)

--- src/com/googlecode/totallylazy/Classes.java
+++ src/com/googlecode/totallylazy/Classes.java
@@ -1,4 +1,13 @@
 package com.googlecode.totallylazy;
 
+import com.googlecode.totallylazy.predicates.LogicalPredicate;
+
 public class Classes {
+    public final LogicalPredicate<Class<?>> isInstance(final Object instance) {
+        return new LogicalPredicate<Class<?>>() {
+            public boolean matches(Class<?> aClass) {
+                return aClass.isInstance(instance);
+            }
+        };
+    }
 }
