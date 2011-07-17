2f6b75a015e24ea706486bd163c05fe1c915188c
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-17 20:05:54 +0100

    Refactored all range queries to use AbstractMapping

--- src/com/googlecode/totallylazy/records/lucene/mappings/AbstractMapping.java
+++ src/com/googlecode/totallylazy/records/lucene/mappings/AbstractMapping.java
@@ -1,9 +1,18 @@
 package com.googlecode.totallylazy.records.lucene.mappings;
 
 import com.googlecode.totallylazy.LazyException;
+import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Query;
 
 public abstract class AbstractMapping<T> implements Mapping<T> {
+    public Query notNull(String name) {
+        try {
+            return newRange(name, null, null, true, true);
+        } catch (Exception e) {
+            throw new LazyException(e);
+        }
+    }
+
     public Query equalTo(String name, T value) {
         try {
             return newRange(name, value, value, true, true);
@@ -15,6 +24,38 @@ public abstract class AbstractMapping<T> implements Mapping<T> {
     public Query greaterThan(String name, T value) {
         try {
             return newRange(name, value, null, false, true);
+        } catch (Exception e) {
+            throw new LazyException(e);
+        }
+    }
+
+    public Query greaterThanOrEqual(String name, T value) {
+        try {
+            return newRange(name, value, null, true, true);
+        } catch (Exception e) {
+            throw new LazyException(e);
+        }
+    }
+
+    public Query lessThan(String name, T value) {
+        try {
+            return newRange(name, null, value, true, false);
+        } catch (Exception e) {
+            throw new LazyException(e);
+        }
+    }
+
+    public Query lessThanOrEqual(String name, T value) {
+        try {
+            return newRange(name, null, value, true, true);
+        } catch (Exception e) {
+            throw new LazyException(e);
+        }
+    }
+
+    public Query between(String name, T lower, T upper) {
+        try {
+            return newRange(name, lower, upper, true, true);
         } catch (Exception e) {
             throw new LazyException(e);
         }
