c47a945460431d93498bde80d0cc89b4d0e906f2
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-19 12:06:27 +0100

    Clean up around LuceneRecords

--- src/com/googlecode/totallylazy/records/lucene/RecordSequence.java
+++ src/com/googlecode/totallylazy/records/lucene/RecordSequence.java
@@ -8,6 +8,7 @@ import com.googlecode.totallylazy.records.lucene.mappings.Mappings;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.store.Directory;
 
+import java.io.PrintStream;
 import java.util.Iterator;
 
 import static com.googlecode.totallylazy.records.lucene.Lucene.and;
@@ -17,22 +18,24 @@ public class RecordSequence extends Sequence<Record> {
     private final Mappings mappings;
     private final Sequence<Keyword> definitions;
     private final Query query;
+    private final PrintStream printStream;
     private final Lucene lucene;
 
-    public RecordSequence(Directory directory, Mappings mappings, Sequence<Keyword> definitions, Query query) {
+    public RecordSequence(Directory directory, Mappings mappings, Sequence<Keyword> definitions, Query query, PrintStream printStream) {
         this.directory = directory;
         this.mappings = mappings;
         this.definitions = definitions;
         this.query = query;
+        this.printStream = printStream;
         lucene = new Lucene(this.mappings);
     }
 
     public Iterator<Record> iterator() {
-        return new DocumentIterator(directory, mappings, definitions, query);
+        return new DocumentIterator(directory, mappings, definitions, query, printStream);
     }
 
     @Override
     public Sequence<Record> filter(Predicate<? super Record> predicate) {
-        return new RecordSequence(directory, mappings, definitions, and(query, lucene.query(predicate)));
+        return new RecordSequence(directory, mappings, definitions, and(query, lucene.query(predicate)), printStream);
     }
 }
--- test/com/googlecode/totallylazy/records/lucene/LuceneRecordsTest.java
+++ test/com/googlecode/totallylazy/records/lucene/LuceneRecordsTest.java
@@ -6,9 +6,12 @@ import com.googlecode.totallylazy.records.lucene.mappings.Mappings;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.NIOFSDirectory;
 import org.apache.lucene.util.Version;
 import org.junit.Ignore;
+
+import javax.sound.midi.SysexMessage;
 
 import static com.googlecode.totallylazy.Files.temporaryDirectory;
 
@@ -16,8 +19,8 @@ public class LuceneRecordsTest extends AbstractRecordsTests {
     @Override
     protected Records createRecords() throws Exception {
         Version version = Version.LUCENE_33;
-        final NIOFSDirectory directory = new NIOFSDirectory(temporaryDirectory());
+        final Directory directory = new NIOFSDirectory(temporaryDirectory());
         IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(version, new StandardAnalyzer(version)));
-        return new LuceneRecords(directory, writer, new Mappings());
+        return new LuceneRecords(directory, writer, new Mappings(), System.out);
     }
 }
