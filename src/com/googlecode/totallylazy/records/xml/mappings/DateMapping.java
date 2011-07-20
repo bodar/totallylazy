76d2574876d787bd177983421c1d94f57d622713
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-20 22:03:25 +0100

    Fixed xml date formating to default to use rfc3339

--- src/com/googlecode/totallylazy/records/xml/mappings/DateMapping.java
+++ src/com/googlecode/totallylazy/records/xml/mappings/DateMapping.java
@@ -4,16 +4,19 @@ import com.googlecode.totallylazy.Sequence;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateMapping implements Mapping<Date> {
-    private final LongMapping mapping = new LongMapping();
+    private final StringMapping mapping = new StringMapping();
+    private final SimpleDateFormat rfc3339 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     public Sequence<Node> to(Document document, String expression, Date value) {
-        return mapping.to(document, expression, value.getTime());
+        return mapping.to(document, expression, rfc3339.format(value));
     }
 
-    public Date from(Sequence<Node> nodes) {
-        return new Date(mapping.from(nodes));
+    public Date from(Sequence<Node> nodes) throws ParseException {
+        return rfc3339.parse(mapping.from(nodes));
     }
 }
