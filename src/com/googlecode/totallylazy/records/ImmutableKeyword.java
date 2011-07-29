c6c8f4272fac1c5751507001e68509102a0ac7d8
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-29 21:08:41 +0100

    Made Keyword an interface

---
+++ src/com/googlecode/totallylazy/records/ImmutableKeyword.java
@@ -0,0 +1,46 @@
+package com.googlecode.totallylazy.records;
+
+public class ImmutableKeyword<T> implements Keyword<T>{
+    private final String name;
+    private final Class<T> aClass;
+
+    public ImmutableKeyword(String name, Class<T> aClass) {
+        if(name == null){
+            throw new IllegalArgumentException("name");
+        }
+        this.name = name;
+        this.aClass = aClass;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof ImmutableKeyword && value().equalsIgnoreCase(((Keyword<T>) other).value());
+    }
+
+    @Override
+    public int hashCode() {
+        return value().toLowerCase().hashCode();
+    }
+
+    public T call(Record record) throws Exception {
+        return record.get(this);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Class<T> forClass() {
+        return aClass;
+    }
+
+    public String value() {
+        return name;
+    }
+
+    // TODO Rename to 'as' somehow
+    public AliasedKeyword<T> alias(Keyword<T> keyword) {
+        return new AliasedKeyword<T>(this, keyword.value());
+    }
+}
--- src/com/googlecode/totallylazy/records/Keywords.java
+++ src/com/googlecode/totallylazy/records/Keywords.java
@@ -12,18 +12,18 @@ public class Keywords {
     }
 
     public static Keyword<Object> keyword(String value) {
-        return new Keyword<Object>(value, value, Object.class);
+        return new ImmutableKeyword<Object>(value, Object.class);
     }
 
     public static <T> Keyword<T> keyword(String value, Class<T> aClass) {
-        return new Keyword<T>(value, value, aClass);
+        return new ImmutableKeyword<T>(value, aClass);
     }
 
     public static Keyword<Object> keyword(String fullyQualifiedName, String name) {
-        return new Keyword<Object>(fullyQualifiedName, name, Object.class);
+        return new ImmutableKeyword<Object>(name, Object.class);
     }
 
     public static <T> Keyword<T> keyword(String fullyQualifiedName, String name, Class<T> aClass) {
-        return new Keyword<T>(fullyQualifiedName, name, aClass);
+        return new ImmutableKeyword<T>(name, aClass);
     }
 }
