0d169a8b4cfd3a61824bfca71d80c860f59b821c
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-30 20:52:50 +0100

    Made SqlKeyword a little more explicit in what it does

--- src/com/googlecode/totallylazy/records/Keyword.java
+++ src/com/googlecode/totallylazy/records/Keyword.java
@@ -6,4 +6,7 @@ import com.googlecode.totallylazy.Value;
 
 public interface Keyword<T> extends Callable1<Record, T>, GenericType<T>, Value<String> {
     String value();
+
+    // TODO Rename to 'as' somehow
+    AliasedKeyword<T> alias(Keyword<T> keyword);
 }
