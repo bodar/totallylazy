f4352da6ead092b26e89772b7cc5588c6027109b
Daniel Worthington-Bodart <dan@bodar.com>
2011-07-31 08:12:33 +0100

    Cleaned up AbstractKeyword

--- src/com/googlecode/totallylazy/records/AliasedKeyword.java
+++ src/com/googlecode/totallylazy/records/AliasedKeyword.java
@@ -2,13 +2,23 @@ package com.googlecode.totallylazy.records;
 
 public class AliasedKeyword<T> extends AbstractKeyword<T>{
     private final Keyword<T> source;
+    private final String name;
 
     public AliasedKeyword(Keyword<T> source, String name) {
-        super(name, source.forClass());
         this.source = source;
+        this.name = name;
     }
 
     public Keyword source() {
         return source;
     }
+
+    public String name() {
+        return name;
+    }
+
+    public Class<T> forClass() {
+        return source.forClass();
+    }
+
 }
