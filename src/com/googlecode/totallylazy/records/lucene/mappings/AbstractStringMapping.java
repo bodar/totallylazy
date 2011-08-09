d7ebfdd1cec9950f2a7b39e0e28a5687427a5275
Daniel Worthington-Bodart <dan@bodar.com>
2011-08-09 09:51:44 +0100

    LuceneRecords: Fixed subtle range bug and added lexical mappings

--- src/com/googlecode/totallylazy/records/lucene/mappings/AbstractStringMapping.java
+++ src/com/googlecode/totallylazy/records/lucene/mappings/AbstractStringMapping.java
@@ -6,8 +6,6 @@ import org.apache.lucene.document.Fieldable;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermRangeQuery;
 
-import static org.apache.lucene.queryParser.QueryParser.escape;
-
 public abstract class AbstractStringMapping<T> extends AbstractMapping<T> {
     private Field.Index index;
 
@@ -37,6 +35,6 @@ public abstract class AbstractStringMapping<T> extends AbstractMapping<T> {
 
     @Override
     protected Query newRange(String name, T lower, T upper, boolean minInclusive, boolean maxInclusive) throws Exception {
-        return new TermRangeQuery(name, lower == null ? null : toString(lower), upper == null ? null : toString(upper), maxInclusive, maxInclusive);
+        return new TermRangeQuery(name, lower == null ? null : toString(lower), upper == null ? null : toString(upper), minInclusive, maxInclusive);
     }
 }
