04f7eea483e47184d997940e459b3a4780f54c7e
dan <dan@bodar.com>
2011-08-18 14:23:55 +0100

    Rewrote transpose in functional manner

--- src/com/googlecode/totallylazy/iterators/TransposeIterator.java
+++ src/com/googlecode/totallylazy/iterators/TransposeIterator.java
@@ -1,41 +1,24 @@
 package com.googlecode.totallylazy.iterators;
 
 import com.googlecode.totallylazy.Iterators;
-import com.googlecode.totallylazy.Pair;
 import com.googlecode.totallylazy.Sequence;
-import com.googlecode.totallylazy.Sequences;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
 
-import static com.googlecode.totallylazy.Pair.pair;
+import static com.googlecode.totallylazy.Sequences.sequence;
 
 public final class TransposeIterator<T> extends ReadOnlyIterator<Sequence<T>> {
-    private final List<Iterator<T>> iterators;
+    private final Sequence<Iterator<T>> iterators;
 
-    public TransposeIterator(List<Iterator<T>> iterators) {
-        this.iterators = iterators;
+    public TransposeIterator(Iterable<Iterator<T>> iterators) {
+        this.iterators = sequence(iterators).realise();
     }
 
     public final boolean hasNext() {
-        for (Iterator<T> iterator : iterators) {
-            if(!iterator.hasNext()){
-                return false;
-            }
-        }
-        return true;
+        return iterators.forAll(Iterators.<T>hasNext());
     }
 
     public final Sequence<T> next() {
-        if (hasNext()) {
-            List<T> result = new ArrayList<T>();
-            for (Iterator<T> iterator : iterators) {
-                result.add(iterator.next());
-            }
-            return Sequences.sequence(result);
-        }
-        throw new NoSuchElementException();
+        return iterators.map(Iterators.<T>next()).realise();
     }
 }
