11361c4c3efedc53100d9e02b2dbb648bd732dd5
Daniel Worthington-Bodart <dan@bodar.com>
2011-08-31 17:27:10 +0100

    Added support for non-standard jar URLs

--- test/com/googlecode/totallylazy/UriTest.java
+++ test/com/googlecode/totallylazy/UriTest.java
@@ -1,10 +1,12 @@
 package com.googlecode.totallylazy;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Uri.uri;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class UriTest {
     @Test
@@ -16,7 +18,18 @@ public class UriTest {
         assertThat(uri.query(), is("foo=bar"));
         assertThat(uri.fragment(), is("Related"));
     }
-    
+
+    @Test
+    public void handlesNonStandardJarUris() throws Exception {
+        Uri uri = uri("jar:http://www.foo.com/bar/baz.jar?foo=bar!/COM/foo/Quux.class");
+        assertThat(uri.scheme(), is("jar"));
+        assertThat(uri.authority(), is("http://www.foo.com/bar/baz.jar?foo=bar"));
+        assertThat(uri.path(), is("/COM/foo/Quux.class"));
+        assertThat(uri.query(), is(nullValue()));
+        assertThat(uri.fragment(), is(nullValue()));
+        assertThat(uri.toString(), is("jar:http://www.foo.com/bar/baz.jar?foo=bar!/COM/foo/Quux.class"));
+    }
+
     @Test
     public void supportsToString() throws Exception {
         assertThat(uri("http://www.ics.uci.edu:80/pub/ietf/uri/?foo=bar#Related").toString(), is("http://www.ics.uci.edu:80/pub/ietf/uri/?foo=bar#Related"));
