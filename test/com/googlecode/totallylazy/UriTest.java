526a44e586dfff4ece95a774ac3ed8b1977ac860
Daniel Worthington-Bodart <dan@bodar.com>
2011-09-27 15:43:12 +0100

    Uri: Improved mergePath logic

--- test/com/googlecode/totallylazy/UriTest.java
+++ test/com/googlecode/totallylazy/UriTest.java
@@ -43,9 +43,11 @@ public class UriTest {
     @Test
     public void supportsMergingPaths() throws Exception {
         assertThat(uri("http://www.ics.uci.edu:80/pub/ietf/uri/?foo=bar#Related").mergePath("/bob").toString(), is("http://www.ics.uci.edu:80/bob?foo=bar#Related"));
+        assertThat(uri("foo/bar").mergePath("bob").toString(), is("foo/bob"));
         assertThat(uri("#Related").mergePath("bob").toString(), is("bob#Related"));
         assertThat(uri("relative?foo=bar").mergePath("bob").toString(), is("bob?foo=bar"));
         assertThat(uri("/pub/ietf/uri/?foo=bar#Related").mergePath("bob").toString(), is("/pub/ietf/uri/bob?foo=bar#Related"));
+        assertThat(uri("/pub/ietf/uri/?foo=bar#Related").mergePath("/bob").toString(), is("/bob?foo=bar#Related"));
     }
 
     @Test
