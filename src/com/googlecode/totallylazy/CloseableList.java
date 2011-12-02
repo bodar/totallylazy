9960ce4f2226c0e79d7906399bd0c2325c5c3e0e
Daniel Worthington-Bodart <dan@bodar.com>
2011-12-02 10:04:26 +0000

    Ensure LuceneIterators close even when they don't iterate to the end

--- src/com/googlecode/totallylazy/CloseableList.java
+++ src/com/googlecode/totallylazy/CloseableList.java
@@ -6,10 +6,16 @@ import java.util.ArrayList;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
 
-public class CloseableList extends ArrayList<Closeable> implements Closeable{
+public class CloseableList extends ArrayList<Closeable> implements Closeable {
     private static final long serialVersionUID = 6663452581122892189L;
+
     public void close() throws IOException {
         sequence(this).each(Closeables.close());
         clear();
+    }
+
+    public <T extends Closeable> T manage(T instance) {
+        add(instance);
+        return instance;
     }
 }
