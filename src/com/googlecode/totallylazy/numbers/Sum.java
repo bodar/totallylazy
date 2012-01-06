8c6592dbd310a4bfe5087c39ecde6237e7feb5fd
Daniel Worthington-Bodart <dan@bodar.com>
2012-01-06 09:54:00 +0000

    Cleanup generics around number

--- src/com/googlecode/totallylazy/numbers/Sum.java
+++ src/com/googlecode/totallylazy/numbers/Sum.java
@@ -2,8 +2,8 @@ package com.googlecode.totallylazy.numbers;
 
 import com.googlecode.totallylazy.Function2;
 
-public class Sum<T extends Number> extends Function2<T, T, Number> {
-    public Number call(T a, T b) {
+public class Sum extends Function2<Number, Number, Number> {
+    public Number call(Number a, Number b) {
         return Numbers.add(a, b);
     }
 }
