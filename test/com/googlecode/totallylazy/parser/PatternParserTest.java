7c298b51953435e676023a9b7cb2e89fcc4052de
dan <dan@bodar.com>
2012-01-25 17:37:52 +0000

    Unfinished regex support

---
+++ test/com/googlecode/totallylazy/parser/PatternParserTest.java
@@ -0,0 +1,18 @@
+package com.googlecode.totallylazy.parser;
+
+import org.junit.Test;
+
+import static com.googlecode.totallylazy.Sequences.characters;
+import static com.googlecode.totallylazy.Unchecked.cast;
+import static com.googlecode.totallylazy.matchers.Matchers.is;
+import static com.googlecode.totallylazy.parser.CharacterParser.character;
+import static com.googlecode.totallylazy.parser.PatternParser.pattern;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PatternParserTest {
+    @Test
+    public void canParseUsingRegex() throws Exception {
+        Result<String> result = pattern("\\d{4}/\\d{1,2}/\\d{1,2}").parse(characters("1977/1/10"));
+        assertThat(result.value(), is("1977/1/10"));
+    }
+}
