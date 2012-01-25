7cc3327f47e7d06a053305e95ec6756467c7a660
dan <dan@bodar.com>
2012-01-25 17:12:46 +0000

    Added sequence parser

---
+++ src/com/googlecode/totallylazy/parser/SequenceParser.java
@@ -0,0 +1,63 @@
+package com.googlecode.totallylazy.parser;
+
+import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.Sequences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.googlecode.totallylazy.Unchecked.cast;
+
+public class SequenceParser<A> extends BaseParser<Sequence<A>> {
+    private final Sequence<? extends Parser<? extends A>> parsers;
+
+    private SequenceParser(Sequence<? extends Parser<? extends A>> parsers) {
+        this.parsers = parsers;
+    }
+
+    public static <A> SequenceParser<A> sequenceOf(final Sequence<? extends Parser<? extends A>> parsers) {
+        return new SequenceParser<A>(parsers);
+    }
+
+    public static <A> SequenceParser<A> sequenceOf(final Parser<? extends A> a, final Parser<? extends A> b) {
+        return new SequenceParser<A>(Sequences.sequence(a, b));
+    }
+
+    public static <A> SequenceParser<A> sequenceOf(final Parser<? extends A> a, final Parser<? extends A> b, final Parser<? extends A> c) {
+        return new SequenceParser<A>(Sequences.sequence(a, b, c));
+    }
+
+    public static <A> SequenceParser<A> sequenceOf(final Parser<? extends A> a, final Parser<? extends A> b, final Parser<? extends A> c, final Parser<? extends A> d) {
+        return new SequenceParser<A>(Sequences.sequence(a, b, c, d));
+    }
+
+    public static <A> SequenceParser<A> sequenceOf(final Parser<? extends A> a, final Parser<? extends A> b, final Parser<? extends A> c, final Parser<? extends A> d, final Parser<? extends A> e) {
+        return new SequenceParser<A>(Sequences.sequence(a, b, c, d, e));
+    }
+
+    public static <A> SequenceParser<A> sequenceOf(final Parser<? extends A>... parsers) {
+        return new SequenceParser<A>(Sequences.sequence(parsers));
+    }
+
+    @Override
+    public String toString() {
+        return parsers.toString();
+    }
+
+    @Override
+    public Result<Sequence<A>> parse(Sequence<Character> characters) throws Exception {
+        Sequence<Character> state = characters;
+        List<A> parsed = new ArrayList<A>();
+        for (Parser<? extends A> parser : parsers) {
+            Result<? extends A> result = parser.parse(state);
+            if(result instanceof Success){
+                Success<A> success = cast(result);
+                parsed.add(success.value());
+                state = success.remainder();
+            } else {
+                return cast(result);
+            }
+        }
+        return Success.success(Sequences.sequence(parsed),state);
+    }
+}
