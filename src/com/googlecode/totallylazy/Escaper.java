41912f361b34ebb0407fa1a8dde881d0af8b6f06
Daniel Worthington-Bodart <dan@bodar.com>
2012-02-07 21:43:00 +0000

    Extracted Rules class and simplified generics of Rule

--- src/com/googlecode/totallylazy/Escaper.java
+++ src/com/googlecode/totallylazy/Escaper.java
@@ -1,20 +1,16 @@
 package com.googlecode.totallylazy;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 import static com.googlecode.totallylazy.Callables.asString;
 import static com.googlecode.totallylazy.Function1.returns1;
 import static com.googlecode.totallylazy.Predicates.always;
 import static com.googlecode.totallylazy.Predicates.is;
 import static com.googlecode.totallylazy.Sequences.characters;
-import static com.googlecode.totallylazy.Sequences.sequence;
 
 public class Escaper {
-    private final Deque<Rule<Character, Character, String>> rules = new ArrayDeque<Rule<Character, Character, String>>();
+    private final Rules<Character, String> rules = new Rules<Character, String>();
 
     public Escaper() {
-        rules.add(Rule.<Character, Character, String>rule(always(Character.class), asString(Character.class)));
+        rules.add(always(Character.class), asString(Character.class));
     }
 
     public Escaper withRule(Character appliesTo, final String result) {
@@ -22,7 +18,7 @@ public class Escaper {
     }
 
     public Escaper withRule(Predicate<? super Character> appliesTo, Callable1<? super Character, ? extends String> action) {
-        rules.addFirst(Rule.<Character, Character, String>rule(appliesTo, action));
+        rules.add(appliesTo, action);
         return this;
     }
 
@@ -33,10 +29,7 @@ public class Escaper {
     private Function1<Character, String> escape() {
         return new Function1<Character, String>() {
             public String call(Character character) throws Exception {
-                return sequence(rules).
-                        filter(Predicates.matches(character)).
-                        head().
-                        call(character);
+                return rules.apply(character);
             }
         };
     }
