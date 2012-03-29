ead1028da9f2c7e05c794c439d526721918974e3
Tom Dan Raymond <Tom Dan Raymond@localhost>
2012-03-29 11:06:34 +0100

    added test to show selecting values without namespace in xpath

--- test/com/googlecode/totallylazy/XmlTest.java
+++ test/com/googlecode/totallylazy/XmlTest.java
@@ -16,10 +16,11 @@ public class XmlTest {
                 "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\" \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">\n" +
                         "<xh:html xmlns:xh=\"http://www.w3.org/1999/xhtml\" xmlns:og=\"http://opengraphprotocol.org/schema/\" xml:lang=\"en-GB\">\n" +
                         "<xh:head><xh:meta content=\"Foo\"/><xh:meta content=\"Bar\"/></xh:head>" +
+                        "<og:boo>far</og:boo>" +
                         "</xh:html>");
 
-        Sequence<String> values = Xml.selectNodes(document, "//meta/@content").map(Xml.contents());
-        assertThat(values, hasExactly("Foo", "Bar"));
+        assertThat(Xml.selectNodes(document, "//meta/@content").map(Xml.contents()), hasExactly("Foo", "Bar"));
+        assertThat(Xml.selectContents(document, "//boo/text()"), is("far")); // this should without text()
     }
 
     @Test
