c097ff409abacb7dc59098fe2980164910427ce2
Daniel Worthington-Bodart <dan@bodar.com>
2012-04-03 21:05:05 +0100

    Made Either/Right/Left use polymorphic dispatch

--- src/com/googlecode/totallylazy/Either.java
+++ src/com/googlecode/totallylazy/Either.java
@@ -2,14 +2,12 @@ package com.googlecode.totallylazy;
 
 import java.util.NoSuchElementException;
 
-import static com.googlecode.totallylazy.Callers.call;
-
 public abstract class Either<L, R> implements Mappable<R, Either<L, ?>> {
-    public static <L,R> Either<L,R> right(R value) {
+    public static <L, R> Either<L, R> right(R value) {
         return Right.right(value);
     }
 
-    public static <L,R> Either<L,R> left(L value) {
+    public static <L, R> Either<L, R> left(L value) {
         return Left.left(value);
     }
 
@@ -21,35 +19,26 @@ public abstract class Either<L, R> implements Mappable<R, Either<L, ?>> {
         return false;
     }
 
-    public R right(){
+    public R right() {
         throw new NoSuchElementException();
     }
 
-    public L left(){
+    public L left() {
         throw new NoSuchElementException();
     }
 
-    public <S> S fold(final S seed, final Callable2<? super S, ? super L, ? extends S> left, final Callable2<? super S, ? super R, ? extends S> right) {
-        return isLeft() ? call(left, seed, left()) : call(right, seed, right());
-    }
+    public abstract <S> S fold(final S seed, final Callable2<? super S, ? super L, ? extends S> left, final Callable2<? super S, ? super R, ? extends S> right);
 
-    public <S> S map(final Callable1<? super L, S> left, final Callable1<? super R, ? extends S> right) {
-        return isLeft() ? call(left, left()) : call(right, right());
-    }
+    public abstract <S> S map(final Callable1<? super L, S> left, final Callable1<? super R, ? extends S> right);
 
     @Override
-    public <S> Either<L, S> map(Callable1<? super R, ? extends S> callable) {
-        return isLeft() ? Either.<L, S>left(left()) : Either.<L, S>right(call(callable, right()));
-    }
+    public abstract <S> Either<L, S> map(Callable1<? super R, ? extends S> callable);
 
-    public <S> Either<L, S> flatMap(Callable1<? super R, ? extends Either<L, S>> callable) {
-        return isLeft() ? Either.<L, S>left(left()) : call(callable, right());
-    }
+    public abstract <S> Either<L, S> flatMap(Callable1<? super R, ? extends Either<L, S>> callable);
 
-    public static <L,R> Either<L,R> flatten(final Either<L, Either<L,R>> either) {
-        return either.flatMap(Function1.<Either<L,R>>identity());
+    public static <L, R> Either<L, R> flatten(final Either<L, Either<L, R>> either) {
+        return either.flatMap(Function1.<Either<L, R>>identity());
     }
-
 
     public abstract Object value();
 }
