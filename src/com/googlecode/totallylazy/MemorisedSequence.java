4184addea5e07df470c4c54c82bf3edd2ffd8165
Daniel Worthington-Bodart <dan@bodar.com>
2012-04-03 16:19:15 +0100

    Cleaned up iterator

--- src/com/googlecode/totallylazy/MemorisedSequence.java
+++ src/com/googlecode/totallylazy/MemorisedSequence.java
@@ -1,10 +1,12 @@
 package com.googlecode.totallylazy;
 
 import com.googlecode.totallylazy.iterators.MemorisedIterator;
+import com.googlecode.totallylazy.iterators.ReadOnlyListIterator;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import static com.googlecode.totallylazy.Unchecked.cast;
 import static java.util.Collections.synchronizedList;
@@ -18,7 +20,7 @@ public final class MemorisedSequence<T> extends Sequence<T> {
         this.iterable = iterable;
     }
 
-    public final Iterator<T> iterator() {
+    public final ReadOnlyListIterator<T> iterator() {
         return new MemorisedIterator<T>(getIterator(), memory);
     }
 
