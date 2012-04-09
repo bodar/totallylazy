acbc74697333b5ee349dfe646baa1d475c4542a4
Daniel Worthington-Bodart <dan@bodar.com>
2012-04-09 19:48:23 +0100

    Minor clean up

--- test/com/googlecode/totallylazy/XPathFunctionsTest.java
+++ test/com/googlecode/totallylazy/XPathFunctionsTest.java
@@ -1,38 +1,39 @@
 package com.googlecode.totallylazy;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
+import static com.googlecode.totallylazy.Xml.document;
+import static com.googlecode.totallylazy.Xml.xpath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class XPathFunctionsTest {
     @Test
     public void supportsJoinStringsInXPath() throws Exception {
-        Document document = Xml.document("<root><node>hello</node><node>world</node></root>");
-        String joinedStrings = Xml.xpath().evaluate("tl:join-strings(//node/text(), '--')", document);
+        Document document = document("<root><node>hello</node><node>world</node></root>");
+        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
         assertThat(joinedStrings, equalTo("hello--world"));
     }
 
     @Test
     public void supportsJoinStringsInXPathOnOneNode() throws Exception {
-        Document document = Xml.document("<root><node>foo</node></root>");
-        String joinedStrings = Xml.xpath().evaluate("tl:join-strings(//node/text(), '--')", document);
+        Document document = document("<root><node>foo</node></root>");
+        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
         assertThat(joinedStrings, equalTo("foo"));
     }
 
     @Test
     public void supportsJoinStringsInXPathWithNoNodes() throws Exception {
-        Document document = Xml.document("<root/>");
-        String joinedStrings = Xml.xpath().evaluate("tl:join-strings(//node/text(), '--')", document);
+        Document document = document("<root/>");
+        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
         assertThat(joinedStrings, equalTo(""));
     }
 
     @Test
     public void supportsJoinStringsInXPathWithNestedNodes() throws Exception {
-        Document document = Xml.document("<root><node><foo>hello</foo></node><node><foo>world</foo></node></root>");
-        String joinedStrings = Xml.xpath().evaluate("tl:join-strings(//node, '--')", document);
+        Document document = document("<root><node><foo>hello</foo></node><node><foo>world</foo></node></root>");
+        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
         assertThat(joinedStrings, equalTo("hello--world"));
     }
 }
