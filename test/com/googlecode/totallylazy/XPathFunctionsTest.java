c56518b129b2571a0e29344240a195ef09ad9988
Tom Malone <tom.malone@mac.com>
2012-04-10 14:20:23 +0100

    renamed join-strings to string-join to be more like xpath 2.0

--- test/com/googlecode/totallylazy/XPathFunctionsTest.java
+++ test/com/googlecode/totallylazy/XPathFunctionsTest.java
@@ -13,35 +13,35 @@ public class XPathFunctionsTest {
     @Test
     public void supportsNewLineChar() throws Exception {
         Document document = document("<root><node>hello</node><node>world</node></root>");
-        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '\\n')", document);
+        String joinedStrings = xpath().evaluate("tl:string-join(//node, '\\n')", document);
         assertThat(joinedStrings, equalTo("hello\nworld"));
     }
 
     @Test
     public void supportsJoinStringsInXPath() throws Exception {
         Document document = document("<root><node>hello</node><node>world</node></root>");
-        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
+        String joinedStrings = xpath().evaluate("tl:string-join(//node, '--')", document);
         assertThat(joinedStrings, equalTo("hello--world"));
     }
 
     @Test
     public void supportsJoinStringsInXPathOnOneNode() throws Exception {
         Document document = document("<root><node>foo</node></root>");
-        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
+        String joinedStrings = xpath().evaluate("tl:string-join(//node, '--')", document);
         assertThat(joinedStrings, equalTo("foo"));
     }
 
     @Test
     public void supportsJoinStringsInXPathWithNoNodes() throws Exception {
         Document document = document("<root/>");
-        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
+        String joinedStrings = xpath().evaluate("tl:string-join(//node, '--')", document);
         assertThat(joinedStrings, equalTo(""));
     }
 
     @Test
     public void supportsJoinStringsInXPathWithNestedNodes() throws Exception {
         Document document = document("<root><node><foo>hello</foo></node><node><foo>world</foo></node></root>");
-        String joinedStrings = xpath().evaluate("tl:join-strings(//node, '--')", document);
+        String joinedStrings = xpath().evaluate("tl:string-join(//node, '--')", document);
         assertThat(joinedStrings, equalTo("hello--world"));
     }
 }
