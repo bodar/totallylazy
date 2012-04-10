4a56d5c14274b5b6d49eb0288540c14b0e5a1a0a
Raymond Barlow <raymanoz@users.noreply.github.com>
2012-04-11 00:24:18 +0100

    first cut. more tests to write.

---
+++ src/com/googlecode/totallylazy/PersistentList.java
@@ -0,0 +1,206 @@
+package com.googlecode.totallylazy;
+
+import com.googlecode.totallylazy.iterators.EmptyIterator;
+import com.googlecode.totallylazy.iterators.ReadOnlyIterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static com.googlecode.totallylazy.Predicates.in;
+import static com.googlecode.totallylazy.Predicates.is;
+import static com.googlecode.totallylazy.Predicates.not;
+import static com.googlecode.totallylazy.Predicates.onlyOnce;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.Sets.set;
+import static com.googlecode.totallylazy.Unchecked.cast;
+
+public abstract class PersistentList<T> implements Iterable<T> {
+    private static final Empty EMPTY = new Empty();
+
+    public static <T> PersistentList<T> empty() {
+        return cast(EMPTY);
+    }
+
+    public static <T> Cons<T> cons(T head, PersistentList<T> tail) {
+        return new Cons<T>(head, tail);
+    }
+
+    public static <T> PersistentList<T> list(T one) {
+        return cons(one, PersistentList.<T>empty());
+    }
+
+    public static <T> PersistentList<T> list(T one, T two) {
+        return cons(one, cons(two, PersistentList.<T>empty()));
+    }
+
+    public static <T> PersistentList<T> list(T one, T two, T three) {
+        return cons(one, cons(two, cons(three, PersistentList.<T>empty())));
+    }
+
+    public static <T> PersistentList<T> list(T one, T two, T three, T four) {
+        return cons(one, cons(two, cons(three, cons(four, PersistentList.<T>empty()))));
+    }
+
+    public static <T> PersistentList<T> list(T one, T two, T three, T four, T five) {
+        return cons(one, cons(two, cons(three, cons(four, cons(five, PersistentList.<T>empty())))));
+    }
+
+    public static <T> PersistentList<T> list(T... values) {
+        return list(sequence(values));
+    }
+
+    public static <T> PersistentList<T> list(Iterable<T> values) {
+        return sequence(values).foldRight(PersistentList.<T>empty(), PersistentList.<T>cons().flip());
+    }
+
+    public abstract T head();
+
+    public abstract PersistentList<T> tail();
+
+    public abstract int size();
+
+    public abstract boolean isEmpty();
+
+    public PersistentList<T> cons(T head) {
+        return cons(head, this);
+    }
+
+    public PersistentList<T> remove(T value) {
+        return list(toSequence().filter(not(onlyOnce(is(value)))));
+    }
+
+    public Sequence<T> toSequence() {
+        return sequence(this);
+    }
+
+    public PersistentList<T> removeAll(Iterable<T> values) {
+        return list(toSequence().filter(not(in(set(values)))));
+    }
+
+    public static <T> Function2<PersistentList<T>, T, PersistentList<T>> cons() {
+        return new Function2<PersistentList<T>, T, PersistentList<T>>() {
+            @Override
+            public PersistentList<T> call(PersistentList<T> ts, T t) throws Exception {
+                return cons(t, ts);
+            }
+        };
+    }
+
+    public PersistentList<T> add(T value) {
+        return list(toSequence().add(value));
+    }
+
+    public List<T> toList() {
+        return toSequence().toList();
+    }
+
+    private static class Empty<T> extends PersistentList<T> {
+        private Empty() {
+        }
+
+        @Override
+        public T head() {
+            throw new NoSuchElementException();
+        }
+
+        @Override
+        public PersistentList<T> tail() {
+            throw new NoSuchElementException();
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return new EmptyIterator<T>();
+        }
+
+        @Override
+        public String toString() {
+            return "[]";
+        }
+    }
+
+    private static class Cons<T> extends PersistentList<T> {
+        private final T head;
+        private final PersistentList<T> tail;
+        private final int size;
+
+        private Cons(T head, PersistentList<T> tail) {
+            this.head = head;
+            this.tail = tail;
+            size = 1 + tail.size();
+        }
+
+        @Override
+        public T head() {
+            return head;
+        }
+
+        @Override
+        public PersistentList<T> tail() {
+            return tail;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return new ConsIterator<T>(this);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s::%s", head, tail);
+        }
+
+        @Override
+        public int hashCode() {
+            return 19 * head.hashCode() * tail.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof Cons ? ((Cons) obj).head.equals(head) && ((Cons) obj).tail.equals(tail) : false;
+        }
+    }
+
+    private static class ConsIterator<T> extends ReadOnlyIterator<T> {
+        private PersistentList<T> list;
+
+        public ConsIterator(PersistentList<T> list) {
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !list.isEmpty();
+        }
+
+        @Override
+        public T next() {
+            final T head = list.head();
+            list = list.tail();
+            return head;
+        }
+    }
+
+
+}
---
+++ test/com/googlecode/totallylazy/XmlTest.java
@@ -0,0 +1,62 @@
+package com.googlecode.totallylazy;
+
+import com.googlecode.totallylazy.matchers.NumberMatcher;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import static com.googlecode.totallylazy.Sequences.repeat;
+import static com.googlecode.totallylazy.matchers.IterableMatcher.hasExactly;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class XmlTest {
+    @Test
+    public void canLoadHtml() throws Exception {
+        Document document = Xml.document(
+                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\" \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">\n" +
+                        "<xh:html xmlns:xh=\"http://www.w3.org/1999/xhtml\" xmlns:og=\"http://opengraphprotocol.org/schema/\" xml:lang=\"en-GB\">\n" +
+                        "<xh:head><xh:meta content=\"Foo\"/><xh:meta content=\"Bar\"/></xh:head>" +
+                        "<og:boo>far</og:boo>" +
+                        "</xh:html>");
+
+        assertThat(Xml.selectNodes(document, "//meta/@content").map(Xml.contents()), hasExactly("Foo", "Bar"));
+        assertThat(Xml.selectContents(document, "//boo/text()"), is("far")); // this should work without text()
+    }
+
+    @Test
+    public void canEscapeXml() throws Exception {
+        assertThat(Xml.escape("& < > ' \" " + new Character((char) 0x80)), is("&amp; &lt; &gt; &#39; &quot; &#128;"));
+    }
+
+    @Test
+    public void doesNotTruncateString() throws Exception {
+        String testString = longStringWithoutEncodedChars();
+        assertThat(Xml.escape(testString), is(testString));
+    }
+
+    private String longStringWithoutEncodedChars() {
+        return repeat("A").take(100).toString("", "", "", Long.MAX_VALUE);
+    }
+
+    @Test
+    public void supportsXPathExpressionsWithFunctionsThatReturnStrings() throws Exception{
+        Document document = Xml.document("<root><child type=\"name\" value=\"bob\"/></root>");
+        String value = Xml.selectContents(document, "concat(//child/@type, ':', //child/@value)");
+        assertThat(value, is("name:bob"));
+    }
+
+    @Test
+    public void supportsReturningANumber() throws Exception{
+        Document document = Xml.document("<root><child type=\"name\" value=\"bob\"/></root>");
+        Number value = Xml.selectNumber(document, "count(//child)");
+        assertThat(value, NumberMatcher.is(1));
+    }
+
+    @Test
+    public void supportsReturningABoolean() throws Exception{
+        Document document = Xml.document("<root><child type=\"name\" value=\"bob\"/></root>");
+        boolean value = Xml.matches(document, "count(//child) = 1");
+        assertThat(value, is(true));
+    }
+}
