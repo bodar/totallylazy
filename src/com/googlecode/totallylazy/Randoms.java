488af7ff7dde30f0b71794cf914f1fd7e07ce5f1
Raymond Barlow <raymanoz@users.noreply.github.com>
2012-04-13 14:11:39 +0100

    Changes randoms to accepts start and end in any order. Added more tests.

--- src/com/googlecode/totallylazy/Randoms.java
+++ src/com/googlecode/totallylazy/Randoms.java
@@ -1,14 +1,17 @@
 package com.googlecode.totallylazy;
 
+import com.googlecode.totallylazy.comparators.Comparators;
 import com.googlecode.totallylazy.iterators.StatefulIterator;
 import com.googlecode.totallylazy.time.Dates;
 import com.googlecode.totallylazy.time.Days;
 
-import java.util.Calendar;
 import java.util.Date;
 
+import static com.googlecode.totallylazy.Pair.pair;
+import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.Sequences.sort;
+import static com.googlecode.totallylazy.time.Dates.stripTime;
 import static java.lang.Math.abs;
-import static java.util.Calendar.DAY_OF_YEAR;
 
 public class Randoms {
     private static java.util.Random random = new java.util.Random();
@@ -23,77 +26,11 @@ public class Randoms {
         });
     }
 
-    public static Sequence<Integer> range(final int start, final int end) {
-        checkRange(start, end);
-        return Sequences.forwardOnly(new StatefulIterator<Integer>() {
-            @Override
-            protected Integer getNext() throws Exception {
-                Integer range = end - start + 1;
-                return random.nextInt(Math.max(1, abs(range))) + start;
-            }
-        });
-    }
-
     public static Sequence<Long> longs() {
         return Sequences.forwardOnly(new StatefulIterator<Long>() {
             @Override
             protected Long getNext() throws Exception {
                 return random.nextLong();
-            }
-        });
-    }
-
-    public static <T> Sequence<T> values(final T... values) {
-        return Sequences.forwardOnly(new StatefulIterator<T>() {
-            @Override
-            protected T getNext() throws Exception {
-                return values[random.nextInt(values.length)];
-            }
-        });
-    }
-
-    public static Sequence<Date> dates() {
-        return Sequences.forwardOnly(new StatefulIterator<Date>() {
-            @Override
-            protected Date getNext() throws Exception {
-                return stripTime(new Date(random.nextLong()));
-            }
-        });
-    }
-
-    public static Sequence<Date> range(final Date start, final Date end) {
-        checkRange(start.getTime(), end.getTime());
-        return Sequences.forwardOnly(new StatefulIterator<Date>() {
-            @Override
-            protected Date getNext() throws Exception {
-                return stripTime(Days.add(start, range(0, daysBetween(start, end)).head()));
-            }
-
-            private Integer daysBetween(Date start, Date end) {
-                return (int) (end.getTime() - start.getTime()) / (24 * 60 * 60 * 1000);
-            }
-        });
-    }
-
-    private static Date stripTime(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTime();
-    }
-
-    public static Sequence<Date> after(final Date date) {
-        return range(date, new Date(Long.MAX_VALUE));
-    }
-
-    public static Sequence<Boolean> booleans() {
-        return Sequences.forwardOnly(new StatefulIterator<Boolean>() {
-            @Override
-            protected Boolean getNext() throws Exception {
-                return random.nextBoolean();
             }
         });
     }
@@ -107,21 +44,74 @@ public class Randoms {
         });
     }
 
-    public static Sequence<Double> range(final Double start, final Double end) {
-        checkRange(start, end);
-        return Sequences.forwardOnly(new StatefulIterator<Double>() {
+    public static Sequence<Boolean> booleans() {
+        return Sequences.forwardOnly(new StatefulIterator<Boolean>() {
             @Override
-            protected Double getNext() throws Exception {
-                return start + random.nextDouble() * (end - start);
+            protected Boolean getNext() throws Exception {
+                return random.nextBoolean();
             }
         });
     }
 
-    private static void checkRange(double start, double end) {
-        if (start > end) throw new IllegalArgumentException("start must be <= end");
+    public static Sequence<Date> dates() {
+        return Sequences.forwardOnly(new StatefulIterator<Date>() {
+            @Override
+            protected Date getNext() throws Exception {
+                return stripTime(new Date(random.nextLong()));
+            }
+        });
     }
 
-    private static void checkRange(long start, long end) {
-        if (start > end) throw new IllegalArgumentException("start must be <= end");
+    public static <T> Sequence<T> values(final T... values) {
+        return Sequences.forwardOnly(new StatefulIterator<T>() {
+            @Override
+            protected T getNext() throws Exception {
+                return values[random.nextInt(values.length)];
+            }
+        });
+    }
+
+    public static Sequence<Integer> between(final int start, final int end) {
+        final Pair<Integer, Integer> bounds = bounds(start, end);
+        return Sequences.forwardOnly(new StatefulIterator<Integer>() {
+            @Override
+            protected Integer getNext() throws Exception {
+                int range = bounds.second() - bounds.first() + 1;
+                return random.nextInt(Math.max(1, abs(range))) + bounds.first();
+            }
+        });
+    }
+
+    public static Sequence<Double> between(final Double start, final Double end) {
+        final Pair<Double, Double> bounds = bounds(start, end);
+        return Sequences.forwardOnly(new StatefulIterator<Double>() {
+            @Override
+            protected Double getNext() throws Exception {
+                return bounds.first() + random.nextDouble() * (bounds.second() - bounds.first());
+            }
+        });
+    }
+
+    public static Sequence<Date> between(final Date start, final Date end) {
+        final Pair<Date, Date> bounds = bounds(start, end);
+        return Sequences.forwardOnly(new StatefulIterator<Date>() {
+            @Override
+            protected Date getNext() throws Exception {
+                return stripTime(Days.add(bounds.first(), between(0, daysBetween(bounds.first(), bounds.second())).head()));
+            }
+
+            private Integer daysBetween(Date start, Date end) {
+                return (int) (end.getTime() - start.getTime()) / (24 * 60 * 60 * 1000);
+            }
+        });
+    }
+
+    public static Sequence<Date> after(final Date date) {
+        return between(date, new Date(Long.MAX_VALUE));
+    }
+
+    private static <T extends Comparable<? super T>> Pair<T, T> bounds(T start, T end) {
+        final Sequence<T> sorted = sort(sequence(start, end), Comparators.<T>ascending());
+        return pair(sorted.first(), sorted.second());
     }
 }
