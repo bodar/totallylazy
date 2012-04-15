edd9e845bfe0077d0f0fb860c6f44eaf397aaf15
Daniel Worthington-Bodart <dan@bodar.com>
2012-04-15 09:32:10 +0100

    Removed duplication and started making Sequence implement same interfaces as Persistent structures

--- src/com/googlecode/totallylazy/Deconstructable.java
+++ src/com/googlecode/totallylazy/Deconstructable.java
@@ -4,6 +4,8 @@ import java.util.NoSuchElementException;
 
 public interface Deconstructable<T, Self extends Deconstructable<T, Self>> {
     boolean isEmpty();
+
     T head() throws NoSuchElementException;
+
     Self tail() throws NoSuchElementException;
 }
