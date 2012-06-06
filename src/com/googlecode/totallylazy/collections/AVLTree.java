43e75e0cd9c851e83bbebd8f58247128bc519cf7
Daniel Worthington-Bodart <dan@bodar.com>
2012-06-06 11:29:39 +0100

    Made AVLTree balanced

--- src/com/googlecode/totallylazy/collections/AVLTree.java
+++ src/com/googlecode/totallylazy/collections/AVLTree.java
@@ -6,10 +6,13 @@ import com.googlecode.totallylazy.comparators.Comparators;
 
 import java.util.Comparator;
 
+import static com.googlecode.totallylazy.Sequences.repeat;
 import static com.googlecode.totallylazy.Unchecked.cast;
 
 public interface AVLTree<K, V> extends ImmutableMap<K, V> {
     int height();
+
+    int balance();
 
     class Empty<K, V> extends EmptyMap<K, V> implements AVLTree<K, V> {
         protected Empty(final Function2<K, V, AVLTree<K, V>> creator) {
@@ -20,35 +23,100 @@ public interface AVLTree<K, V> extends ImmutableMap<K, V> {
         public int height() {
             return 0;
         }
+
+        @Override
+        public int balance() {
+            return 0;
+        }
     }
 
     class Node<K, V> extends TreeMap<K, V> implements AVLTree<K, V> {
         private final int height;
+        final AVLTree<K, V> left;
+        final AVLTree<K, V> right;
 
-        Node(AVLTree<K, V> left, K key, V value, AVLTree<K, V> right, Comparator<K> comparator) {
+        private Node(AVLTree<K, V> left, K key, V value, AVLTree<K, V> right, Comparator<K> comparator) {
             super(left, key, value, right, comparator);
+            this.left = left;
+            this.right = right;
             height = Math.max(left.height(), right.height()) + 1;
+        }
+
+        static <K, V> Node<K, V> node(AVLTree<K, V> left, K key, V value, AVLTree<K, V> right, Comparator<K> comparator) {
+            return new Node<K, V>(left, key, value, right, comparator);
         }
 
         @Override
         <K, V> Node<K, V> create(ImmutableMap<K, V> left, K key, V value, ImmutableMap<K, V> right, Comparator<K> comparator) {
-            AVLTree<K, V> leftAvl = Unchecked.<AVLTree<K, V>>cast(left);
-            AVLTree<K, V> rightAvl = Unchecked.<AVLTree<K, V>>cast(right);
-            return balance(new Node<K, V>(leftAvl, key, value, rightAvl, comparator));
+            return balance(node(Unchecked.<AVLTree<K, V>>cast(left), key, value, Unchecked.<AVLTree<K, V>>cast(right), comparator));
         }
 
-        private static <K,V> Node<K,V> balance(Node<K,V> node) {
-            if(balanceFactor())
+        // http://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/AVL_Tree_Rebalancing.svg/350px-AVL_Tree_Rebalancing.svg.png
+        private static <K, V> Node<K, V> balance(Node<K, V> parent) {
+            if (parent.balance() == -2) {
+                if (parent.right.balance() == 1) {
+                    return balanceRightLeft(parent);
+                }
+                if (parent.right.balance() == -1) {
+                    return balanceRightRight(parent);
+                }
+            } else if(parent.balance() == 2) {
+                if(parent.left.balance() == -1){
+                    return balanceLeftRight(parent);
+                }
+                if(parent.left.balance() == 1){
+                    return balanceLeftLeft(parent);
+                }
+            }
+            return parent;
         }
 
-        private static <K, V> int balanceFactor(Node<K,V> node) {
-            return node.left.height() - rightAvl.height();
+        private static <K, V> Node<K, V> balanceLeftLeft(Node<K, V> parent) {
+            AVLTree<K, V> c = asNode(parent.left).right;
+            Node<K, V> five = parent.left(c);
+            return asNode(parent.left).right(five);
         }
 
+        private static <K, V> Node<K, V> balanceLeftRight(Node<K, V> parent) {
+            AVLTree<K, V> b = asNode(asNode(parent.left).right).left;
+            Node<K, V> three = asNode(parent.left).right(b);
+            Node<K, V> four = asNode(asNode(parent.left).right).left(three);
+            return balance(parent.left(four));
+        }
+
+        private static <K, V> Node<K, V> balanceRightRight(Node<K, V> parent) {
+            AVLTree<K, V> b = asNode(parent.right).left;
+            Node<K, V> three = parent.right(b);
+            return asNode(parent.right).left(three);
+        }
+
+        private static <K, V> Node<K, V> balanceRightLeft(Node<K, V> parent) {
+            AVLTree<K, V> c = asNode(asNode(parent.right).left).right;
+            Node<K, V> five = asNode(parent.right).left(c);
+            Node<K, V> four = asNode(asNode(parent.right).left).right(five);
+            return balance(parent.right(four));
+        }
+
+        private static <K, V> Node<K, V> asNode(AVLTree<K, V> node) {
+            return cast(node);
+        }
 
         @Override
         public int height() {
             return height;
+        }
+
+        @Override
+        public int balance() {
+            return left.height() - right.height();
+        }
+
+        private Node<K, V> left(AVLTree<K, V> newLeft) {
+            return node(newLeft, key, value, right, comparator);
+        }
+
+        private Node<K, V> right(AVLTree<K, V> newRight) {
+            return node(left, key, value, newRight, comparator);
         }
     }
 
@@ -70,7 +138,7 @@ public interface AVLTree<K, V> extends ImmutableMap<K, V> {
         }
 
         public static <K, V> Node<K, V> node(AVLTree<K, V> left, K k, V v, AVLTree<K, V> right, final Comparator<K> comparator) {
-            return new Node<K, V>(left, k, v, right, comparator);
+            return Node.node(left, k, v, right, comparator);
         }
     }
 
--- test/com/googlecode/totallylazy/collections/AVLTreeTest.java
+++ test/com/googlecode/totallylazy/collections/AVLTreeTest.java
@@ -1,15 +1,134 @@
 package com.googlecode.totallylazy.collections;
 
+import com.googlecode.totallylazy.Callable2;
+import com.googlecode.totallylazy.Files;
+import com.googlecode.totallylazy.Maps;
+import com.googlecode.totallylazy.Option;
+import com.googlecode.totallylazy.Randoms;
+import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.callables.TimeCallable;
+import com.googlecode.totallylazy.callables.TimeReport;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
 
 import static com.googlecode.totallylazy.collections.AVLTree.constructors.node;
 import static com.googlecode.totallylazy.matchers.Matchers.is;
+import static com.googlecode.totallylazy.numbers.Numbers.range;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AVLTreeTest {
     @Test
-    public void balances() throws Exception {
-        ImmutableMap<Integer, Object> map = node(3, null).put(4, null).put(5, null);
-        assertThat(map.toString(), is("(( 3 ) 4 ( 5 ))"));
+    public void balancesRightRightCase() throws Exception {
+        assertThat(node(3, null).put(4, null).put(5, null).toString(), is("(( 3 ) 4 ( 5 ))"));
+    }
+
+    @Test
+    public void balancesRightLeftCase() throws Exception {
+        assertThat(node(3, null).put(5, null).put(4, null).toString(), is("(( 3 ) 4 ( 5 ))"));
+    }
+
+    @Test
+    public void balancesLeftLeftCase() throws Exception {
+        assertThat(node(5, null).put(4, null).put(3, null).toString(), is("(( 3 ) 4 ( 5 ))"));
+    }
+
+    @Test
+    public void balancesLeftRightCase() throws Exception {
+        assertThat(node(5, null).put(3, null).put(4, null).toString(), is("(( 3 ) 4 ( 5 ))"));
+    }
+
+    @Test
+    @Ignore("Manual")
+    public void performanceDoesNotSuck() throws Exception {
+//
+//        BEGIN MUTABLE
+//        Elapsed time: 1.323778 msecs
+//        Elapsed msecs for 1001 runs:	Avg:5.320724094881408E-4	Min:3.48E-4	Max:0.021052	Total:0.5585599999999998
+//        END MUTABLE
+//        BEGIN IMMUTABLE
+//        Elapsed time: 4.220562 msecs
+//        Elapsed msecs for 1001 runs:	Avg:4.6022222222222205E-4	Min:2.06E-4	Max:0.013102	Total:0.49465499999999996
+//        END IMMUTABLE
+
+        
+        final Sequence<Integer> range = range(1, 10000).safeCast(Integer.class).realise();
+        final Sequence<Integer> keys = Randoms.between(0, 10000);
+
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("BEGIN MUTABLE");
+            final Map<Integer, String> mutable = TimeCallable.time(new Callable<Map<Integer, String>>() {
+                @Override
+                public Map<Integer, String> call() throws Exception {
+                    return range.fold(new TreeMap<Integer, String>() {{
+                                          put(0, "0");
+                                      }}, new Callable2<Map<Integer, String>, Integer, Map<Integer, String>>() {
+                                          @Override
+                                          public Map<Integer, String> call(Map<Integer, String> map, Integer integer) throws Exception {
+                                              map.put(integer, integer.toString());
+                                              return map;
+                                          }
+                                      }
+                    );
+                }
+            }).call();
+
+            TimeReport time2 = TimeReport.time(1000, new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    Integer key = keys.head();
+                    Option<String> value = Maps.get(mutable, key);
+                    return value;
+                }
+            });
+            System.out.println(time2);
+            System.out.println("END MUTABLE");
+
+
+            System.out.println("BEGIN IMMUTABLE");
+            final ImmutableMap<Integer, String> map = TimeCallable.time(new Callable<ImmutableMap<Integer, String>>() {
+                @Override
+                public ImmutableMap<Integer, String> call() throws Exception {
+                    return range.fold(node(0, "0"), new Callable2<ImmutableMap<Integer, String>, Integer, ImmutableMap<Integer, String>>() {
+                        @Override
+                        public ImmutableMap<Integer, String> call(ImmutableMap<Integer, String> node, Integer integer) throws Exception {
+                            return node.put(integer, integer.toString());
+                        }
+                    });
+                }
+            }).call();
+
+            TimeReport time = TimeReport.time(1000, new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    Integer key = keys.head();
+                    Option<String> value = map.get(key);
+                    return value;
+                }
+            });
+            System.out.println(time);
+            System.out.println("END IMMUTABLE");
+
+        }
+
+
+//        Files.write(("<html><head><style>td { text-align: center; border: 1px solid gray; }</style></head><body>" + new ImmutableMapRenderer().render((AVLTree<?, ?>) map) + "</body></html>").getBytes(), new File("/home/dev/tree.html"));
+    }
+
+
+    private class ImmutableMapRenderer {
+        public String render(AVLTree<?, ?> map) {
+            if (map instanceof AVLTree.Node) {
+                return "<table>" + "<tr><td colspan='2'>" + ((AVLTree.Node) map).key + "</td></tr>" +
+                        "<tr><td  valign='top'>" + render(((AVLTree.Node) map).left) + "</td><td valign='top'>" + render(((AVLTree.Node) map).right) + "</td></tr></table>";
+            }
+            return "";
+        }
     }
 }
