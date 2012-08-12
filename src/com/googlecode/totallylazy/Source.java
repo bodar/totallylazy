3a591fb077a1b40ffafc7aae5c1a0aff6e82aac8
Daniel Worthington-Bodart <dan@bodar.com>
2012-08-12 23:10:57 +0100

    Seperated copy and copyAndClose from Source

--- src/com/googlecode/totallylazy/Source.java
+++ src/com/googlecode/totallylazy/Source.java
@@ -4,29 +4,32 @@ import java.io.Closeable;
 import java.io.InputStream;
 
 import static com.googlecode.totallylazy.Closeables.using;
-import static com.googlecode.totallylazy.Streams.copyAndClose;
 
 public interface Source extends Closeable {
     Sequence<Pair<String, InputStream>> sources();
 
     class methods {
-        public static Integer copy(Source source, final Destination destination) {
+        public static int copyAndClose(Source source, final Destination destination) {
             return using(source, new Function1<Source, Integer>() {
                 @Override
                 public Integer call(final Source source) throws Exception {
                     return using(destination, new Callable1<Destination, Integer>() {
                         @Override
                         public Integer call(final Destination destination) throws Exception {
-                            return source.sources().map(new Function1<Pair<String, InputStream>, Void>() {
-                                @Override
-                                public Void call(Pair<String, InputStream> pair) throws Exception {
-                                    return copyAndClose(pair.second(), destination.destination(pair.first()));
-                                }
-                            }).size();
+                            return copy(source, destination);
                         }
                     });
                 }
             });
+        }
+
+        public static int copy(Source source, final Destination destination) {
+            return source.sources().map(new Function1<Pair<String, InputStream>, Void>() {
+                @Override
+                public Void call(Pair<String, InputStream> pair) throws Exception {
+                    return Streams.copyAndClose(pair.second(), destination.destination(pair.first()));
+                }
+            }).size();
         }
     }
 }
