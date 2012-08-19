8dbacb771a7e7c0bbb1006a249677c0cd82b1cad
Daniel Worthington-Bodart <dan@bodar.com>
2012-08-19 20:23:06 +0100

    Added reflection methods and more overloads for and and or predicates

--- src/com/googlecode/totallylazy/Predicates.java
+++ src/com/googlecode/totallylazy/Predicates.java
@@ -149,6 +149,18 @@ public class Predicates {
         return and(Sequences.<Predicate<? super T>>sequence(first, second));
     }
 
+    public static <T> LogicalPredicate<T> and(final Predicate<? super T> first, final Predicate<? super T> second, final Predicate<? super T> third) {
+        return and(Sequences.<Predicate<? super T>>sequence(first, second, third));
+    }
+
+    public static <T> LogicalPredicate<T> and(final Predicate<? super T> first, final Predicate<? super T> second, final Predicate<? super T> third, final Predicate<? super T> fourth) {
+        return and(Sequences.<Predicate<? super T>>sequence(first, second, third, fourth));
+    }
+
+    public static <T> LogicalPredicate<T> and(final Predicate<? super T> first, final Predicate<? super T> second, final Predicate<? super T> third, final Predicate<? super T> fourth, final Predicate<? super T> fifth) {
+        return and(Sequences.<Predicate<? super T>>sequence(first, second, third, fourth, fifth));
+    }
+
     public static <T> LogicalPredicate<T> and(final Predicate<? super T>... predicates) {
         return and(sequence(predicates));    }
 
@@ -162,6 +174,18 @@ public class Predicates {
 
     public static <T> LogicalPredicate<T> or(final Predicate<? super T> first, final Predicate<? super T> second) {
         return or(Sequences.<Predicate<? super T>>sequence(first, second));
+    }
+
+    public static <T> LogicalPredicate<T> or(final Predicate<? super T> first, final Predicate<? super T> second, final Predicate<? super T> third) {
+        return or(Sequences.<Predicate<? super T>>sequence(first, second, third));
+    }
+
+    public static <T> LogicalPredicate<T> or(final Predicate<? super T> first, final Predicate<? super T> second, final Predicate<? super T> third, final Predicate<? super T> fourth) {
+        return or(Sequences.<Predicate<? super T>>sequence(first, second, third, fourth));
+    }
+
+    public static <T> LogicalPredicate<T> or(final Predicate<? super T> first, final Predicate<? super T> second, final Predicate<? super T> third, final Predicate<? super T> fourth, final Predicate<? super T> fifth) {
+        return or(Sequences.<Predicate<? super T>>sequence(first, second, third, fourth, fifth));
     }
 
     public static <T> LogicalPredicate<T> or(final Predicate<? super T>... predicates) {
