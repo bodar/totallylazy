a408c65e122f06e9cdb7ebb36a0c2378544ded97
dan and tom <dan and tom@localhost>
2012-08-22 17:45:10 +0100

    Added xpath tokenize support

--- src/com/googlecode/totallylazy/XPathFunctions.java
+++ src/com/googlecode/totallylazy/XPathFunctions.java
@@ -1,6 +1,10 @@
 package com.googlecode.totallylazy;
 
+import com.googlecode.totallylazy.regex.Regex;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
 import javax.xml.namespace.QName;
 import javax.xml.xpath.XPathFunction;
@@ -21,6 +25,34 @@ public class XPathFunctions {
         add(signature("trim-and-join", argumentsOf(NodeList.class, String.class)), nodeListAndString(trimAndJoin()));
         add(signature("if", argumentsOf(NodeList.class, Object.class, Object.class)), threeNodeLists(ifElse()));
         add(signature("or", Predicates.<List<Object>>all()), returnFirstNonNullOrEmpty());
+        add(signature("tokenize", argumentsOf(NodeList.class, String.class)), stringStringAndNodeList(tokenize()));
+    }
+
+    private static Function2<NodeList, String, NodeList> tokenize() {
+        return new Function2<NodeList, String, NodeList>() {
+            @Override
+            public NodeList call(final NodeList input, final String pattern) throws Exception {
+                return new NodeArrayList<Text>(Xml.sequence(input).flatMap(split(pattern)));
+
+            }
+        };
+    }
+
+    private static Function1<Node, Sequence<Text>> split(final String pattern) {
+        return new Function1<Node, Sequence<Text>>() {
+            @Override
+            public Sequence<Text> call(final Node node) throws Exception {
+                return Regex.regex(pattern).split(node.getTextContent()).map(createText(node));                    }
+        };
+    }
+
+    private static Function1<String, Text> createText(final Node node) {
+        return new Function1<String, Text>() {
+            @Override
+            public Text call(String text) throws Exception {
+                return node.getOwnerDocument().createTextNode(text);
+            }
+        };
     }
 
     private static Function1<List<Object>, Object> returnFirstNonNullOrEmpty() {
@@ -46,8 +78,8 @@ public class XPathFunctions {
         };
     }
 
-    public static Rules<Pair<String, List<Object>>, Object> add(Predicate<Pair<String, List<Object>>> signature, final Function1<List<Object>, Object> callable) {
-        return functions.add(signature, new Function1<Pair<String, List<Object>>, Object>() {
+    public static Rules<Pair<String, List<Object>>, Object> add(Predicate<Pair<String, List<Object>>> signature, final Function1<List<Object>, ?> callable) {
+        return functions.addLast(signature, new Function1<Pair<String, List<Object>>, Object>() {
             @Override
             public Object call(Pair<String, List<Object>> pair) throws Exception {
                 return callable.call(pair.second());
@@ -113,11 +145,20 @@ public class XPathFunctions {
         };
     }
 
-    private static Function1<List<Object>, Object> nodeListAndString(final Callable2<NodeList, String, String> callable) {
-        return new Function1<List<Object>, Object>() {
+    private static Function1<List<Object>, String> nodeListAndString(final Callable2<NodeList, String, String> callable) {
+        return new Function1<List<Object>, String>() {
             @Override
-            public Object call(List<Object> objects) throws Exception {
+            public String call(List<Object> objects) throws Exception {
                 return callable.call((NodeList) objects.get(0), unescape((String) objects.get(1)));
+            }
+        };
+    }
+
+    private static Function1<List<Object>, NodeList> stringStringAndNodeList(final Function2<NodeList, String, NodeList> function) {
+        return new Function1<List<Object>, NodeList>() {
+            @Override
+            public NodeList call(List<Object> objects) throws Exception {
+                return function.call((NodeList) objects.get(0), (String) objects.get(1));
             }
         };
     }
@@ -152,5 +193,23 @@ public class XPathFunctions {
                 });
             }
         };
+    }
+
+    public static class NodeArrayList<T extends Node> implements NodeList {
+        private final List<T> texts;
+
+        public NodeArrayList(Iterable<T> texts) {
+            this.texts = sequence(texts).toList();
+        }
+
+        @Override
+        public Node item(int index) {
+            return texts.get(index);
+        }
+
+        @Override
+        public int getLength() {
+            return texts.size();
+        }
     }
 }
