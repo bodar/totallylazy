d0bcc926c54443e5a44522997632cb1468b90789
dan & raymond <dan & raymond@localhost>
2012-10-04 16:09:16 +0100

    aliased inDebug with debugging

--- src/com/googlecode/totallylazy/Debug.java
+++ src/com/googlecode/totallylazy/Debug.java
@@ -4,6 +4,10 @@ import static java.lang.management.ManagementFactory.getRuntimeMXBean;
 
 public class Debug {
     public static boolean inDebug() {
+        return debugging();
+    }
+
+    public static boolean debugging() {
         return getRuntimeMXBean().getInputArguments().toString().contains("-agentlib:jdwp");
     }
 }
