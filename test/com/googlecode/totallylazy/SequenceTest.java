95f10ab9f0a497d4b6a43b26e0e285ffcf9aeeea
Raymond Barlow <rbarlow@raymanoz.com>
2012-10-11 21:55:39 +0100

    added toOption to Sequence. Added cons function to Sequences

--- test/com/googlecode/totallylazy/SequenceTest.java
+++ test/com/googlecode/totallylazy/SequenceTest.java
@@ -51,6 +51,7 @@ import static com.googlecode.totallylazy.Sequences.repeat;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.Sequences.sort;
 import static com.googlecode.totallylazy.Sequences.splitOn;
+import static com.googlecode.totallylazy.Sequences.toOption;
 import static com.googlecode.totallylazy.Sequences.zip;
 import static com.googlecode.totallylazy.Strings.toCharacters;
 import static com.googlecode.totallylazy.Triple.triple;
@@ -756,5 +757,14 @@ public class SequenceTest {
         assertThat(sequence().intersperse("x"), isEmpty());
         assertThat(repeat(1).intersperse(0).take(5), hasExactly(1, 0, 1, 0, 1));
         assertThat(repeat(1).intersperse(0).take(6), hasExactly(1, 0, 1, 0, 1, 0));
+    }
+
+    @Test
+    public void supportsToOption() {
+        assertThat(sequence("roger", "ramjet").toOption(), is(some(sequence("roger", "ramjet"))));
+        assertThat(toOption(sequence("roger", "ramjet")), is(some(sequence("roger", "ramjet"))));
+
+        assertThat(empty(String.class).toOption(), is(Option.<Sequence<String>>none()));
+        assertThat(toOption(empty(String.class)), is(Option.<Sequence<String>>none()));
     }
 }
