7a6fe1d452f863098528bfb85200679e9534817f
Daniel Worthington-Bodart <dan@bodar.com>
2012-11-05 17:10:13 +0000

    Made 'in' predicate use a lazy collection to improve performance and unify implementations

--- src/com/googlecode/totallylazy/predicates/InPredicate.java
+++ src/com/googlecode/totallylazy/predicates/InPredicate.java
@@ -1,19 +1,42 @@
 package com.googlecode.totallylazy.predicates;
 
-import com.googlecode.totallylazy.Sequence;
+import com.googlecode.totallylazy.Lazy;
+import com.googlecode.totallylazy.Unchecked;
+
+import java.util.Collection;
+
+import static com.googlecode.totallylazy.Sequences.sequence;
+
 
 public class InPredicate<T> extends LogicalPredicate<T> {
-    private final Sequence<T> sequence;
+    private final Iterable<T> original;
+    private final Lazy<Collection<T>> collection;
 
-    public InPredicate(Sequence<T> sequence) {
-        this.sequence = sequence;
+    private InPredicate(final Iterable<T> iterable) {
+        this.collection = new Lazy<Collection<T>>() {
+            @Override
+            protected Collection<T> get() throws Exception {
+                return collection(iterable);
+            }
+        };
+        this.original = iterable;
+    }
+
+    public static <T> InPredicate<T> in(final Iterable<? extends T> iterable) {
+        return new InPredicate<T>(Unchecked.<Iterable<T>>cast(iterable));
+    }
+
+    private Collection<T> collection(Iterable<T> iterable) {
+        if(iterable instanceof Collection) return (Collection<T>) iterable;
+        return sequence(iterable).toList();
     }
 
     public boolean matches(T other) {
-        return sequence.contains(other);
+        return collection.value().contains(other);
     }
 
-    public Sequence<T> values() {
-        return sequence;
+    // Used by LazyRecords, do not inline or change
+    public Iterable<T> values() {
+        return original;
     }
 }
