76b1c7471dfe35ddf983f9d420a521e6e58fc987
dan <dan@bodar.com>
2012-11-12 14:00:59 +0000

    Changed Trie to use option

--- src/com/googlecode/totallylazy/collections/Trie.java
+++ src/com/googlecode/totallylazy/collections/Trie.java
@@ -3,11 +3,13 @@ package com.googlecode.totallylazy.collections;
 import com.googlecode.totallylazy.Option;
 import com.googlecode.totallylazy.Value;
 
-public class Trie<T> implements Value<T> {
-    private final T value;
-    private final ImmutableSortedMap<Character, Trie<T>> children;
+import static com.googlecode.totallylazy.Option.some;
 
-    private Trie(T value, ImmutableSortedMap<Character, Trie<T>> children) {
+public class Trie<V> implements Value<V> {
+    private final Option<V> value;
+    private final ImmutableSortedMap<Character, Trie<V>> children;
+
+    private Trie(Option<V> value, ImmutableSortedMap<Character, Trie<V>> children) {
         this.value = value;
         this.children = children;
     }
@@ -16,33 +18,29 @@ public class Trie<T> implements Value<T> {
         return trie(null);
     }
 
-    public static <T> Trie<T> trie(T value) {
+    public static <T> Trie<T> trie(Option<T> value) {
         return trie(value, ImmutableSortedMap.constructors.<Character, Trie<T>>emptySortedMap());
     }
 
-    public static <T> Trie<T> trie(T value, ImmutableSortedMap<Character, Trie<T>> children) {
+    public static <T> Trie<T> trie(Option<T> value, ImmutableSortedMap<Character, Trie<T>> children) {
         return new Trie<T>(value, children);
     }
 
-    public ImmutableSortedMap<Character, Trie<T>> children() {
-        return children;
-    }
-
-    public T get(CharSequence key) {
-        if(key.length() == 0) return value();
+    public Option<V> get(CharSequence key) {
+        if(key.length() == 0) return value;
         return childFor(key).get().get(tail(key));
     }
 
-    public Trie<T> put(CharSequence key, T value) {
-        if(key.length() == 0) return trie(value, children);
-        return trie(value(), children.put(key.charAt(0), childFor(key).getOrElse(Trie.<T>trie()).put(tail(key), value)));
+    public Trie<V> put(CharSequence key, V value) {
+        if(key.length() == 0) return trie(some(value), children);
+        return trie(this.value, children.put(key.charAt(0), childFor(key).getOrElse(Trie.<V>trie()).put(tail(key), value)));
     }
 
-    public T value() {
-        return value;
+    public V value() {
+        return value.get();
     }
 
-    private Option<Trie<T>> childFor(CharSequence key) {return children.get(key.charAt(0));}
+    private Option<Trie<V>> childFor(CharSequence key) {return children.get(key.charAt(0));}
 
     private CharSequence tail(CharSequence key) {return key.subSequence(1, key.length());}
 }
