0004adbd6196de499623289bea8f66de3f7e60ff
Daniel Worthington-Bodart <dan@bodar.com>
2012-11-12 14:00:59 +0000

    Changed Trie to use option

--- test/com/googlecode/totallylazy/collections/TrieTest.java
+++ test/com/googlecode/totallylazy/collections/TrieTest.java
@@ -2,6 +2,8 @@ package com.googlecode.totallylazy.collections;
 
 import org.junit.Test;
 
+import static com.googlecode.totallylazy.Option.none;
+import static com.googlecode.totallylazy.Option.some;
 import static com.googlecode.totallylazy.collections.ImmutableSortedMap.constructors;
 import static com.googlecode.totallylazy.collections.Trie.trie;
 import static com.googlecode.totallylazy.matchers.Matchers.is;
@@ -10,13 +12,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class TrieTest {
     @Test
     public void get() throws Exception {
-        Trie<String> trie = trie(null, constructors.<Character, Trie<String>>sortedMap('a', trie(null, constructors.<Character, Trie<String>>sortedMap('b', trie("Foo")))));
-        assertThat(trie.get("ab"), is("Foo"));
+        Trie<String> trie = trie(none(String.class), constructors.<Character, Trie<String>>sortedMap('a', trie(none(String.class), constructors.<Character, Trie<String>>sortedMap('b', trie(some("Foo"))))));
+        assertThat(trie.get("ab").get(), is("Foo"));
     }
 
     @Test
     public void put() throws Exception {
         Trie<String> trie = Trie.<String>trie().put("ab", "Foo");
-        assertThat(trie.get("ab"), is("Foo"));
+        assertThat(trie.get("ab").get(), is("Foo"));
     }
 }
