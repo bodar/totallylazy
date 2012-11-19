c8339552ffbe02e6708e6225607d28217a27fa29
Daniel Worthington-Bodart <dan@bodar.com>
2012-11-19 21:05:43 +0000

    Added lcm identity

---
+++ src/com/googlecode/totallylazy/numbers/Lcm.java
@@ -0,0 +1,16 @@
+package com.googlecode.totallylazy.numbers;
+
+import com.googlecode.totallylazy.Function2;
+import com.googlecode.totallylazy.Identity;
+
+public class Lcm extends Function2<Number, Number, Number> implements Identity<Number> {
+    @Override
+    public Number call(Number x, Number y) throws Exception {
+        return Numbers.lcm(x, y);
+    }
+
+    @Override
+    public Number identity() {
+        return 1;
+    }
+}
