955c829e2e1939f8af28dc4bbcf8a7a9d0c5467e
Daniel Worthington-Bodart <dan@bodar.com>
2012-12-06 22:28:44 +0000

    Added minimum and maximum with identity

--- src/com/googlecode/totallylazy/comparators/Maximum.java
+++ src/com/googlecode/totallylazy/comparators/Maximum.java
@@ -1,7 +1,11 @@
 package com.googlecode.totallylazy.comparators;
 
 import com.googlecode.totallylazy.Associative;
+import com.googlecode.totallylazy.CombinerFunction;
 import com.googlecode.totallylazy.Function2;
+import com.googlecode.totallylazy.time.Dates;
+
+import java.util.Date;
 
 public class Maximum<T extends Comparable<? super T>> extends Function2<T, T, T> implements Associative<T> {
     public T call(T a, T b) throws Exception {
@@ -18,5 +22,19 @@ public class Maximum<T extends Comparable<? super T>> extends Function2<T, T, T>
 
     public static <T extends Comparable<? super T>> Function2<T, T, T> maximum() {
         return new Maximum<T>();
+    }
+
+    public static <T extends Comparable<? super T>> CombinerFunction<T> maximum(final T identity) {
+        return new CombinerFunction<T>() {
+            @Override
+            public T call(T t, T t2) throws Exception {
+                return maximum(t, t2);
+            }
+
+            @Override
+            public T identity() {
+                return identity;
+            }
+        };
     }
 }
--- src/com/googlecode/totallylazy/comparators/Minimum.java
+++ src/com/googlecode/totallylazy/comparators/Minimum.java
@@ -1,6 +1,7 @@
 package com.googlecode.totallylazy.comparators;
 
 import com.googlecode.totallylazy.Associative;
+import com.googlecode.totallylazy.CombinerFunction;
 import com.googlecode.totallylazy.Function2;
 
 public class Minimum<T extends Comparable<? super T>> extends Function2<T, T, T> implements Associative<T> {
@@ -19,4 +20,19 @@ public class Minimum<T extends Comparable<? super T>> extends Function2<T, T, T>
     public static <T extends Comparable<? super T>> Function2<T, T, T> minimum() {
         return new Minimum<T>();
     }
+
+    public static <T extends Comparable<? super T>> CombinerFunction<T> minimum(final T identity) {
+        return new CombinerFunction<T>() {
+            @Override
+            public T call(T t, T t2) throws Exception {
+                return minimum(t, t2);
+            }
+
+            @Override
+            public T identity() {
+                return identity;
+            }
+        };
+    }
+
 }
