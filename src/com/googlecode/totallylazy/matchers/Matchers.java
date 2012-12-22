b7ec2d81dcf8c65cfb8c3656590f21b83f03f712
Matt Savage <matthew.savage@gmail.com>
2012-12-22 20:45:22 +0000

    Added Function1<SelfDescribing,String> Matchers.description()

--- src/com/googlecode/totallylazy/matchers/Matchers.java
+++ src/com/googlecode/totallylazy/matchers/Matchers.java
@@ -1,14 +1,17 @@
 package com.googlecode.totallylazy.matchers;
 
 import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Function1;
 import com.googlecode.totallylazy.Predicate;
 import com.googlecode.totallylazy.predicates.LogicalPredicate;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.SelfDescribing;
 import org.hamcrest.TypeSafeMatcher;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.Unchecked.cast;
+import static org.hamcrest.StringDescription.asString;
 
 public class Matchers {
 
@@ -18,6 +21,15 @@ public class Matchers {
 
     public static <T> Iterable<Matcher<T>> are(final Iterable<T> values) {
         return sequence(values).map(Matchers.<T>isMatcher());
+    }
+
+    public static Function1<SelfDescribing, String> description() {
+        return new Function1<SelfDescribing, String>() {
+            @Override
+            public String call(SelfDescribing selfDescribing) throws Exception {
+                return asString(selfDescribing);
+            }
+        };
     }
 
     public static <T> Callable1<T, Matcher<T>> isMatcher(Class<T> clazz) {
