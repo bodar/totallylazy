4d4e5872577f855edef4ec035c86633ea15667cb
Matt Savage <matthew.savage@gmail.com>
2012-12-24 01:22:12 +0000

    Backing out validations until we can work out how to convince JDK 1.6 to compile the classes.

--- src/com/googlecode/totallylazy/validations/AbstractValidator.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.totallylazy.validations;
-
-public abstract class AbstractValidator<T> implements Validator<T> {
-	@Override
-	public boolean matches(T value) {
-		return validate(value).succeeded();
-	}
-}
