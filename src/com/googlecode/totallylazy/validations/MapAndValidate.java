971df1964fc1e4d8b365cdc2048a675855509fc2
Matt Savage <savagematt@users.noreply.github.com>
2012-12-24 00:25:25 +0000

    Added totallylazy.validations (see previous commits. this checkin to try to sidestep javac generics bug on build server)

--- src/com/googlecode/totallylazy/validations/MapAndValidate.java
+++ src/com/googlecode/totallylazy/validations/MapAndValidate.java
@@ -4,7 +4,7 @@ import com.googlecode.totallylazy.Callable1;
 
 import static com.googlecode.totallylazy.Callers.call;
 
-public class MapAndValidate<T, R> extends LogicalValidator<T> {
+public class MapAndValidate<R, T> extends LogicalValidator<T> {
 	private final Callable1<T, R> map;
 	private final Validator<? super R> validator;
 
@@ -20,8 +20,8 @@ public class MapAndValidate<T, R> extends LogicalValidator<T> {
 	}
 
 	public static class constructors{
-		public static <T,R> MapAndValidate<T,R> mapAndValidate(Callable1<T, R> map, Validator<? super R> validator) {
-			return new MapAndValidate<T,R>(map, validator);
+		public static <T,R> MapAndValidate<R, T> mapAndValidate(Callable1<T, R> map, Validator<? super R> validator) {
+			return new MapAndValidate<R, T>(map, validator);
 		}
 	}
 }
