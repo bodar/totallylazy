99309677fcd9543cafe8f037275a10df7564dcb8
Matt Savage <matthew.savage@gmail.com>
2013-01-05 14:07:44 +0000

    Removed FlatMapAndValidate in favour of the more appropriate EveryItemValidator. Fixed generics in MapAndValidate for edge cases.

--- src/com/googlecode/totallylazy/validations/MapAndValidate.java
+++ src/com/googlecode/totallylazy/validations/MapAndValidate.java
@@ -5,10 +5,10 @@ import com.googlecode.totallylazy.Callable1;
 import static com.googlecode.totallylazy.Callers.call;
 
 public class MapAndValidate<T, R> extends LogicalValidator<T> {
-	private final Callable1<T, R> map;
+	private final Callable1<? super T, ? extends R> map;
 	private final Validator<? super R> validator;
 
-	public MapAndValidate(Callable1<T, R> map, Validator<? super R> validator) {
+	public MapAndValidate(Callable1<? super T, ? extends R> map, Validator<? super R> validator) {
 		this.map = map;
 		this.validator = validator;
 	}
@@ -20,7 +20,7 @@ public class MapAndValidate<T, R> extends LogicalValidator<T> {
 	}
 
 	public static class constructors{
-		public static <T,R> MapAndValidate<T, R> mapAndValidate(Callable1<T, R> map, Validator<? super R> validator) {
+		public static <T,R> MapAndValidate<T, R> mapAndValidate(Callable1<? super T, ? extends R> map, Validator<? super R> validator) {
 			return new MapAndValidate<T, R>(map, validator);
 		}
 	}
