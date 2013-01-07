a5bd57c2ce4cb54c910220f63aad4b7a4254274a
Matt Savage <savagematt@users.noreply.github.com>
2013-01-07 19:55:07 +0000

    Fixing generics in ConditionalValidatorTest

--- test/com/googlecode/totallylazy/validations/ConditionalValidatorTest.java
+++ test/com/googlecode/totallylazy/validations/ConditionalValidatorTest.java
@@ -1,6 +1,5 @@
 package com.googlecode.totallylazy.validations;
 
-import com.googlecode.totallylazy.predicates.LogicalPredicate;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Predicates.is;
@@ -10,18 +9,16 @@ import static org.junit.Assert.assertTrue;
 public class ConditionalValidatorTest {
     @Test
     public void returnsResultOfValidatorIfPredicateIsMatched() {
-        LogicalPredicate<String> isB = is("B");
-        LogicalValidator<String> conditionalValidator = validateThat(is("A")).
-                when(isB);
+        LogicalValidator<String> conditionalValidator = validateThat(is("A")).castValidator(String.class).
+                when(is("B"));
 
         assertTrue(conditionalValidator.validate("B").failed());
     }
 
     @Test
     public void doesNotValidateIfPredicateIsNotMatched() {
-        LogicalPredicate<String> isB = is("B");
-        LogicalValidator<String> conditionalValidator = validateThat(is("A")).
-                when(isB);
+        LogicalValidator<String> conditionalValidator = validateThat(is("A")).castValidator(String.class).
+                when(is("B"));
 
         assertTrue(conditionalValidator.validate("C").succeeded());
     }
