f95791b81f703f8be8a1d86d7dea643a3b22674b
Matt Savage <matthew.savage@gmail.com>
2013-01-07 18:23:41 +0000

    Added AnyOfValidator and LogicalValidator.or(Validator<T>)

---
+++ test/com/googlecode/totallylazy/validations/AnyOfValidatorTest.java
@@ -0,0 +1,30 @@
+package com.googlecode.totallylazy.validations;
+
+import org.junit.Test;
+
+import static com.googlecode.totallylazy.Predicates.is;
+import static com.googlecode.totallylazy.matchers.IterableMatcher.hasExactly;
+import static com.googlecode.totallylazy.matchers.IterableMatcher.isEmpty;
+import static com.googlecode.totallylazy.validations.Validators.validateThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AnyOfValidatorTest {
+    @Test
+    public void shouldPassIfAnyValidatorPasses() {
+        Validator<String> validator = validateThat(is("A")).
+                or(validateThat(is("B"))). // or() creates an AnyOfValidator
+                assigningFailuresTo("key");
+
+        assertThat(validator.validate("A").messages("key"), isEmpty(String.class));
+        assertThat(validator.validate("B").messages("key"), isEmpty(String.class));
+    }
+
+    @Test
+    public void shouldFailIfAllValidatorsFail() {
+        Validator<String> validator = validateThat(is("A")).
+                or(validateThat(is("B"))). // or() creates an AnyOfValidator
+                assigningFailuresTo("key");
+
+        assertThat(validator.validate("C").messages("key"), hasExactly(AnyOfValidator.DEFAULT_MESSAGE));
+    }
+}
