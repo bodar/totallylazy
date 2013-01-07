d3b9a26cf147288aed84cae3e2d6aed471265b89
Matt Savage <matthew.savage@gmail.com>
2013-01-07 18:38:02 +0000

    Fixing javac bug

--- test/com/googlecode/totallylazy/validations/AnyOfValidatorTest.java
+++ test/com/googlecode/totallylazy/validations/AnyOfValidatorTest.java
@@ -13,7 +13,8 @@ public class AnyOfValidatorTest {
     public void shouldPassIfAnyValidatorPasses() {
         LogicalValidator<String> validator = validateThat(is("A")).
                 or(validateThat(is("B"))). // or() creates an AnyOfValidator
-                assigningFailuresTo("key");
+                assigningFailuresTo("key").
+                castValidator();
 
         assertThat(validator.validate("A").messages("key"), isEmpty(String.class));
         assertThat(validator.validate("B").messages("key"), isEmpty(String.class));
@@ -23,7 +24,8 @@ public class AnyOfValidatorTest {
     public void shouldFailIfAllValidatorsFail() {
         LogicalValidator<String> validator = validateThat(is("A")).
                 or(validateThat(is("B"))). // or() creates an AnyOfValidator
-                assigningFailuresTo("key");
+                assigningFailuresTo("key").
+                castValidator();
 
         assertThat(validator.validate("C").messages("key"), hasExactly(AnyOfValidator.DEFAULT_MESSAGE));
     }
