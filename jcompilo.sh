95d35b7a6f852db7e08a9ee6c2a4d0e4fd9bbced
Daniel Worthington-Bodart <dan@bodar.com>
2013-01-22 15:46:26 +0000

    Updated jcompilo

--- jcompilo.sh
+++ jcompilo.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-version=94
+version=98
 artifact=jcompilo
 group=com/googlecode/${artifact}
 repo=repo.bodar.com.s3.amazonaws.com
 dir=lib/
 jar=${dir}${artifact}.jar
-url=http://${repo}/${group}/${artifact}/${version}/${artifact}-${version}.jar
+url=http://${repo}/${group}/${artifact}/${version}/${artifact}-${version}
+remote_jar=${url}.jar
+remote_sh=${url}.sh
 
 if [ $1 = "-u" ]; then 
 	rm ${jar}
@@ -15,6 +17,7 @@ fi
 
 if [ ! -f ${jar} ]; then
 	mkdir -p ${dir} 
-	wget ${url} -O ${jar} || curl -o ${jar} ${url}
+	wget -O ${jar} ${remote_jar} || curl -o ${jar} ${remote_jar}
+	wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
 fi
 exec java -jar ${jar} $*
