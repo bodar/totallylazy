1cbc36cd9abc38dc6f1761662b2ab8a24f27fa0f
Daniel Worthington-Bodart <dan@bodar.com>
2013-01-25 14:18:09 +0000

    Added overload with type safe cast

--- src/com/googlecode/totallylazy/Fields.java
+++ src/com/googlecode/totallylazy/Fields.java
@@ -2,6 +2,8 @@ package com.googlecode.totallylazy;
 
 import java.lang.reflect.Field;
 
+import static com.googlecode.totallylazy.Unchecked.cast;
+
 public class Fields {
     public static Mapper<Field, String> name = new Mapper<Field, String>() {
         @Override
@@ -17,13 +19,30 @@ public class Fields {
         }
     };
 
-    public static Mapper<Field, Object> value(final Object instance) {
-        return new Mapper<Field, Object>() {
+    public static <T> Mapper<Field, T> value(final Object instance) {
+        return new Mapper<Field, T>() {
             @Override
-            public Object call(Field field) throws Exception {
-                return access(field).get(instance);
+            public T call(Field field) throws Exception {
+                return get(field, instance);
             }
         };
+    }
+
+    public static <T> Mapper<Field, T> value(final Object instance, final Class<T> aClass) {
+        return new Mapper<Field, T>() {
+            @Override
+            public T call(Field field) throws Exception {
+                return get(field, instance, aClass);
+            }
+        };
+    }
+
+    public static <T> T get(Field field, Object instance) throws IllegalAccessException {
+        return cast(access(field).get(instance));
+    }
+
+    public static <T> T get(Field field, Object instance, Class<T> aClass) throws IllegalAccessException {
+        return aClass.cast(access(field).get(instance));
     }
 
     public static Mapper<Field, Integer> modifiers = new Mapper<Field, Integer>() {
