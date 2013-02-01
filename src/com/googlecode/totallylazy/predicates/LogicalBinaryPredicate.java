12460fd3cd16b964598ce56adf9a5f09a47ef422
Daniel Worthington-Bodart <dan@bodar.com>
2013-02-01 16:48:43 +0000

    Fixing BinaryPredicate

--- src/com/googlecode/totallylazy/predicates/LogicalBinaryPredicate.java
+++ src/com/googlecode/totallylazy/predicates/LogicalBinaryPredicate.java
@@ -1,26 +1,32 @@
 package com.googlecode.totallylazy.predicates;
 
 import com.googlecode.totallylazy.BinaryPredicate;
-import com.googlecode.totallylazy.Function2;
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Callable2;
+import com.googlecode.totallylazy.Function1;
 
-public abstract class LogicalBinaryPredicate<T> extends Function2<T, T, Boolean> implements BinaryPredicate<T> {
+public abstract class LogicalBinaryPredicate<T> extends Function1<T, LogicalPredicate<T>> implements BinaryPredicate<T>, Callable1<T, LogicalPredicate<T>>, Callable2<T, T, Boolean> {
     @Override
     public Boolean call(T t, T t2) throws Exception {
         return matches(t, t2);
     }
 
     @Override
-    public LogicalPredicate<T> apply(T t) {
-        return LogicalPredicate.logicalPredicate(super.apply(t));
+    public LogicalPredicate<T> call(final T a) throws Exception {
+        return new LogicalPredicate<T>() {
+            @Override
+            public boolean matches(T b) {
+                return LogicalBinaryPredicate.this.matches(a, b);
+            }
+        };
     }
 
-    @Override
-    public LogicalPredicate<T> applySecond(T t) {
-        return LogicalPredicate.logicalPredicate(super.applySecond(t));
-    }
-
-    @Override
-    public LogicalBinaryPredicate<T> flip() {
-        return BinaryPredicate.constructors.binary(super.flip());
+    public LogicalPredicate<T> applySecond(final T b) {
+        return new LogicalPredicate<T>() {
+            @Override
+            public boolean matches(T a) {
+                return LogicalBinaryPredicate.this.matches(a, b);
+            }
+        };
     }
 }
