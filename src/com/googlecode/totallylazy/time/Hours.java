3fab93736df173788bd9bc2c348bc645612aa430
Stuart <Stuart@localhost>
2013-02-14 12:06:56 +0000

    Add Hours and Minutes as a counterpart to Seconds

--- src/com/googlecode/totallylazy/time/Hours.java
+++ src/com/googlecode/totallylazy/time/Hours.java
@@ -4,26 +4,27 @@ import com.googlecode.totallylazy.Function2;
 
 import java.util.Date;
 
+import static java.util.Calendar.HOUR;
 import static java.util.Calendar.SECOND;
 
-public class Seconds {
+public class Hours {
     public static Date add(Date date, int amount) {
-        return Dates.add(date, SECOND, amount);
+        return Dates.add(date, HOUR, amount);
     }
 
     public static Date subtract(Date date, int amount) {
-        return Dates.subtract(date, SECOND, amount);
+        return Dates.subtract(date, HOUR, amount);
     }
 
     public static Long between(Date start, Date end) {
-        return (end.getTime() - start.getTime()) / 1000L;
+        return Minutes.between(start, end) / 60;
     }
 
     public static class functions {
         public static Function2<Date, Integer, Date> add = new Function2<Date, Integer, Date>() {
             @Override
             public Date call(Date date, Integer amount) throws Exception {
-                return Seconds.add(date, amount);
+                return Hours.add(date, amount);
             }
         };
 
@@ -34,7 +35,7 @@ public class Seconds {
         public static Function2<Date, Integer, Date> subtract = new Function2<Date, Integer, Date>() {
             @Override
             public Date call(Date date, Integer amount) throws Exception {
-                return Seconds.subtract(date, amount);
+                return Hours.subtract(date, amount);
             }
         };
 
@@ -45,7 +46,7 @@ public class Seconds {
         public static Function2<Date, Date, Long> between = new Function2<Date, Date, Long>() {
             @Override
             public Long call(Date start, Date end) throws Exception {
-                return Seconds.between(start, end);
+                return Hours.between(start, end);
             }
         };
 
