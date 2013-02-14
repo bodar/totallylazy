d7760007e133e8fcb33ce924b70ef438afa746d9
Stuart <spmiller@users.noreply.github.com>
2013-02-14 17:39:43 +0000

    Added accessor functions for DateConverter

--- src/com/googlecode/totallylazy/time/DateConverter.java
+++ src/com/googlecode/totallylazy/time/DateConverter.java
@@ -1,9 +1,45 @@
 package com.googlecode.totallylazy.time;
 
+import com.googlecode.totallylazy.Function1;
+import com.googlecode.totallylazy.Function2;
+
 import java.util.Date;
 
 public interface DateConverter {
     String format(Date value);
 
     Date parse(String value);
+
+    public static class functions {
+        public static Function2<DateConverter, Date, String> format = new Function2<DateConverter, Date, String>() {
+            @Override
+            public String call(DateConverter dateConverter, Date date) throws Exception {
+                return dateConverter.format(date);
+            }
+        };
+
+        public static Function2<DateConverter, Date, String> format()  {
+            return format;
+        }
+
+        public static Function1<Date, String> format(DateConverter dateConverter)  {
+            return format.apply(dateConverter);
+        }
+
+        public static Function2<DateConverter, String, Date> parse = new Function2<DateConverter, String, Date>() {
+            @Override
+            public Date call(DateConverter dateConverter, String dateAsString) throws Exception {
+                return dateConverter.parse(dateAsString);
+            }
+        };
+
+        public static Function2<DateConverter, String, Date> parse() {
+            return parse;
+        }
+
+        public static Function1<String, Date> parse(DateConverter dateConverter)  {
+            return parse.apply(dateConverter);
+        }
+    }
+
 }
