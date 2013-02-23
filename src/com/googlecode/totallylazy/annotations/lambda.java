82d5ea89f158336d7858eef4d2cb627d9d7b8d94
Daniel Worthington-Bodart <dan@bodar.com>
2013-02-23 16:16:53 +0000

    Added lambda annotation and cleaned up other annotations

--- src/com/googlecode/totallylazy/annotations/lambda.java
+++ src/com/googlecode/totallylazy/annotations/lambda.java
@@ -5,7 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** Used by <a href="https://code.google.com/p/jcompilo/">jcompilo</a> to do tail call optimisation */
+/** Used by <a href="https://code.google.com/p/jcompilo/">jcompilo</a> to enable lambda processing */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
-public @interface tailrec {}
+public @interface lambda {
+}
