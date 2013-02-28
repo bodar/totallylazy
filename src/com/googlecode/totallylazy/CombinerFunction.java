900110af82e2549c11cfa7173eabacf0a9e68b2b
Daniel Worthington-Bodart <dan@bodar.com>
2013-02-28 12:01:03 +0000

    Added reducer combiner function

--- src/com/googlecode/totallylazy/CombinerFunction.java
+++ src/com/googlecode/totallylazy/CombinerFunction.java
@@ -1,4 +1,8 @@
 package com.googlecode.totallylazy;
 
 public abstract class CombinerFunction<T> extends BinaryFunction<T> implements Combiner<T> {
+    @Override
+    public T combine(T a, T b) throws Exception {
+        return call(a, b);
+    }
 }
---
+++ src/com/googlecode/totallylazy/ReducerCombinerFunction.java
@@ -0,0 +1,4 @@
+package com.googlecode.totallylazy;
+
+public abstract class ReducerCombinerFunction<T,R> extends ReducerFunction<T,R> implements ReducerCombiner<T,R> {
+}
