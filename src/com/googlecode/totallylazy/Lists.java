9f9456f454ab593c71f8ceca72684a87eb7f82d7
Daniel Worthington-Bodart <dan@bodar.com>
2013-03-15 17:21:19 +0000

    Added fold test to MapContract

--- src/com/googlecode/totallylazy/Lists.java
+++ src/com/googlecode/totallylazy/Lists.java
@@ -1,5 +1,6 @@
 package com.googlecode.totallylazy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
@@ -13,12 +14,37 @@ public class Lists {
         return sequence(iterable).toList();
     }
 
-    public static <T> Function1<T, Integer> indexIn(final List<? extends T> values){
+    public static <T> Function1<T, Integer> indexIn(final List<? extends T> values) {
         return new Function1<T, Integer>() {
             @Override
             public Integer call(T t) throws Exception {
                 return values.indexOf(t);
             }
         };
+    }
+
+    public static class functions {
+        public static <T> ReducerCombinerFunction<T, List<T>> add() {
+            return new ReducerCombinerFunction<T, List<T>>() {
+                @Override
+                public List<T> combine(List<T> a, List<T> b) throws Exception {
+                    List<T> result = identity();
+                    result.addAll(a);
+                    result.addAll(b);
+                    return result;
+                }
+
+                @Override
+                public List<T> call(List<T> rs, T t) throws Exception {
+                    rs.add(t);
+                    return rs;
+                }
+
+                @Override
+                public List<T> identity() {
+                    return new ArrayList<T>();
+                }
+            };
+        }
     }
 }
