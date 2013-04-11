9a59b999174b51f5c0086c60ac76e5e17f57e49b
Daniel Worthington-Bodart <dan@bodar.com>
2013-04-11 17:34:23 +0100

    Renamed function

--- test/com/googlecode/totallylazy/xml/XPathLookupsTest.java
+++ test/com/googlecode/totallylazy/xml/XPathLookupsTest.java
@@ -7,7 +7,6 @@ import org.w3c.dom.Document;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.googlecode.totallylazy.Maps.functions.get;
 import static com.googlecode.totallylazy.Xml.document;
 import static com.googlecode.totallylazy.Xml.xpath;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,7 +18,7 @@ public class XPathLookupsTest {
         Map<String, String> data = new HashMap<String, String>() {{
             put("Dan", "Jorge");
         }};
-        XPathLookups.setLookup("name", get(data));
+        XPathLookups.setLookup("name", Maps.functions.getFrom(data));
         Document document = document("<user>Dan</user>");
         assertThat(xpath().evaluate("tl:lookup('name', //user)", document), equalTo("Jorge"));
     }
