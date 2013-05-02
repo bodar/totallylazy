59041364927116561113f51d23c19be3b8fb55ca
Daniel Worthington-Bodart <dan@bodar.com>
2013-05-02 10:01:06 +0100

    Fixed numbers parameters

--- src/com/googlecode/totallylazy/numbers/Numbers.java
+++ src/com/googlecode/totallylazy/numbers/Numbers.java
@@ -588,6 +588,6 @@ public class Numbers {
     }
 
     public static class parameters {
-        public static int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,_;
+        public static Number a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,_;
     }
 }
