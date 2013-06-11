ef9e988f45ff4849fe72da4ce0628e8721e9d8d6
Raymond Barlow <raymanoz@users.noreply.github.com>
2013-06-11 15:51:14 +0100

    Created static factory methods for clocks

--- src/com/googlecode/totallylazy/time/SystemClock.java
+++ src/com/googlecode/totallylazy/time/SystemClock.java
@@ -3,6 +3,12 @@ package com.googlecode.totallylazy.time;
 import java.util.Date;
 
 public class SystemClock implements Clock {
+    public SystemClock() {}
+
+    public static SystemClock systemClock() {
+        return new SystemClock();
+    }
+
     public Date now() {
         return new Date();
     }
