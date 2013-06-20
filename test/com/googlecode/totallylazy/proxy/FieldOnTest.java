3da77563b92b6837704614f140be7f61a2bdf3f7
Daniel Worthington-Bodart <dan@bodar.com>
2013-06-20 11:58:53 +0100

    Added additional test to demo actual usage

--- test/com/googlecode/totallylazy/proxy/FieldOnTest.java
+++ test/com/googlecode/totallylazy/proxy/FieldOnTest.java
@@ -1,18 +1,27 @@
 package com.googlecode.totallylazy.proxy;
 
+import com.googlecode.totallylazy.Fields;
 import com.googlecode.totallylazy.Sequence;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.totallylazy.matchers.IterableMatcher.hasExactly;
+import static com.googlecode.totallylazy.matchers.Matchers.is;
 import static com.googlecode.totallylazy.proxy.User.user;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FieldOnTest {
     @Test
+    public void capturesField() throws Exception {
+        assertThat(new FieldOn<User, String>() { { get(instance.firstName); } }.field(), is(User.class.getField("firstName")));
+    }
+
+    @Test
     public void canMapWithAField() throws Exception {
         Sequence<User> users = sequence(user("Dan", "Bodart"), user("Matt", "Savage"));
-        Sequence<String> firstNames = users.map(new FieldOn<User, String>(){ { get(instance.firstName); } });
+        Sequence<String> firstNames = users.map(new FieldOn<User, String>() { { get(instance.firstName); } });
         assertThat(firstNames, hasExactly("Dan", "Matt"));
     }
 }
