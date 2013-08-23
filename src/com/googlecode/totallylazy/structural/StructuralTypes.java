4505b1e3beb84087d6c54277e864f389bd4a340f
Daniel Worthington-Bodart <dan@bodar.com>
2013-08-23 21:49:07 +0100

    Minor clean up

--- src/com/googlecode/totallylazy/structural/StructuralTypes.java
+++ src/com/googlecode/totallylazy/structural/StructuralTypes.java
@@ -16,6 +16,9 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Map;
 
+import static com.googlecode.totallylazy.Maps.map;
+import static com.googlecode.totallylazy.Methods.allMethods;
+import static com.googlecode.totallylazy.Pair.pair;
 import static com.googlecode.totallylazy.Sequences.sequence;
 
 public class StructuralTypes {
@@ -25,37 +28,29 @@ public class StructuralTypes {
         return Proxy.createProxy(structuralType, new InvocationHandler() {
             @Override
             public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
-                try {
-                    return methods.get(method).invoke(instance, objects);
-                } catch (IllegalAccessException e) {
-                    throw e;
-                } catch (IllegalArgumentException e) {
-                    throw e;
-                } catch (InvocationTargetException e) {
-                    throw e.getCause();
-                }
+                return Methods.invoke(methods.get(method), instance, objects);
             }
         });
     }
 
     private static <T> Map<Method, Method> extractMethods(final Object instance, Class<T> structuralType) {
-        final Sequence<Method> instanceMethods = Methods.allMethods(instance.getClass());
-        return Maps.map(sequence(structuralType.getMethods()).map(new Mapper<Method, Pair<Method, Method>>() {
+        final Sequence<Method> instanceMethods = allMethods(instance.getClass());
+        return map(sequence(structuralType.getMethods()).map(new Mapper<Method, Pair<Method, Method>>() {
             @Override
             public Pair<Method, Method> call(Method requiredMethod) throws Exception {
-                return Pair.pair(requiredMethod, findMethod(requiredMethod, instanceMethods).get());
+                return pair(requiredMethod, findMethod(requiredMethod, instanceMethods).get());
             }
         }));
     }
 
     private static Option<Method> findMethod(final Method requiredMethod, final Sequence<Method> instanceMethods) {
-        final Sequence<Type> requiredParamaters = sequence(requiredMethod.getGenericParameterTypes());
+        final Sequence<Type> requiredParameters = sequence(requiredMethod.getGenericParameterTypes());
         final Sequence<Type> requiredReturnType = sequence(requiredMethod.getGenericReturnType());
         return instanceMethods.find(new LogicalPredicate<Method>() {
             @Override
             public boolean matches(Method objectsMethod) {
                 return objectsMethod.getName().equals(requiredMethod.getName()) &&
-                        sequence(objectsMethod.getGenericParameterTypes()).equals(requiredParamaters) &&
+                        sequence(objectsMethod.getGenericParameterTypes()).equals(requiredParameters) &&
                         sequence(objectsMethod.getGenericReturnType()).equals(requiredReturnType);
             }
         });
