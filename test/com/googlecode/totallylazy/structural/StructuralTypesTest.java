5d16cf57fbdf21850fd70e3e28c2344ce375367b
Daniel Worthington-Bodart <dan@bodar.com>
2013-08-23 18:44:36 +0100

    Added basic structrual type support

---
+++ test/com/googlecode/totallylazy/structural/StructuralTypesTest.java
@@ -0,0 +1,26 @@
+package com.googlecode.totallylazy.structural;
+
+import org.junit.Test;
+
+import java.io.Closeable;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.googlecode.totallylazy.matchers.Matchers.is;
+import static com.googlecode.totallylazy.structural.StructuralTypes.structrualType;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StructuralTypesTest {
+    @Test
+    public void canCreateStructuralType() throws Exception {
+        final AtomicBoolean called = new AtomicBoolean(false);
+        Object closeable = new Object() {
+            void close() {
+                called.set(true);
+            }
+        };
+
+        Closeable close = structrualType(Closeable.class, closeable);
+        close.close();
+        assertThat(called.get(), is(true));
+    }
+}
