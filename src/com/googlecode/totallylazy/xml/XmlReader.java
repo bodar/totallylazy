f24260af2efa5447b56e230ab7dcd9273cc10014
Daniel Worthington-Bodart <dan@bodar.com>
2013-10-21 14:46:16 +0100

    Added support to use Predicate against QName

--- src/com/googlecode/totallylazy/xml/XmlReader.java
+++ src/com/googlecode/totallylazy/xml/XmlReader.java
@@ -1,12 +1,15 @@
 package com.googlecode.totallylazy.xml;
 
 import com.googlecode.totallylazy.LazyException;
+import com.googlecode.totallylazy.Mapper;
+import com.googlecode.totallylazy.Predicate;
 import com.googlecode.totallylazy.Unchecked;
 import com.googlecode.totallylazy.Xml;
 import com.googlecode.totallylazy.iterators.StatefulIterator;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -18,21 +21,27 @@ import java.io.Reader;
 import java.util.Iterator;
 
 import static com.googlecode.totallylazy.LazyException.lazyException;
+import static com.googlecode.totallylazy.Predicates.is;
+import static com.googlecode.totallylazy.Predicates.where;
 import static com.googlecode.totallylazy.Sequences.forwardOnly;
 
 public class XmlReader extends StatefulIterator<Node> {
     private final XMLEventReader reader;
-    private final String repeatingElement;
+    private final Predicate<? super QName> predicate;
 
-    private XmlReader(XMLEventReader reader, String repeatingElement) {
+    private XmlReader(XMLEventReader reader, Predicate<? super QName> predicate) {
         this.reader = reader;
-        this.repeatingElement = repeatingElement;
+        this.predicate = predicate;
     }
 
-    public static XmlReader xmlReader(Reader reader, String repeatingElement) throws LazyException {
+    public static XmlReader xmlReader(Reader reader, String localName) throws LazyException {
+        return xmlReader(reader, where(functions.localPart, is(localName)));
+    }
+
+    public static XmlReader xmlReader(Reader reader, Predicate<? super QName> predicate) {
         try {
             XMLInputFactory factory = XMLInputFactory.newInstance();
-            return new XmlReader(factory.createXMLEventReader(reader), repeatingElement);
+            return new XmlReader(factory.createXMLEventReader(reader), predicate);
         } catch (XMLStreamException e) {
             throw lazyException(e);
         }
@@ -40,14 +49,12 @@ public class XmlReader extends StatefulIterator<Node> {
 
     @Override
     protected Node getNext() throws Exception {
-        while (reader.hasNext()){
+        while (reader.hasNext()) {
             XMLEvent event = reader.nextEvent();
-
             if (event instanceof StartElement) {
                 StartElement start = (StartElement) event;
-                if (start.getName().getLocalPart().equals(repeatingElement)) {
-                    return children(copyAttributes(start, element(start.getName().getLocalPart())));
-                }
+                QName qName = start.getName();
+                if (predicate.matches(qName)) return children(copyAttributes(start, element(qName.getLocalPart())));
             }
         }
         return finished();
@@ -75,6 +82,16 @@ public class XmlReader extends StatefulIterator<Node> {
             destination.setAttribute(attribute.getName().getLocalPart(), attribute.getValue());
         }
         return destination;
+    }
+
+    public static class functions {
+        public static Mapper<QName, String> localPart = new Mapper<QName, String>() {
+            @Override
+            public String call(QName qName) throws Exception {
+                return qName.getLocalPart();
+            }
+        };
+
     }
 
 }
