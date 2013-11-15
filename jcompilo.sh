32f7e17f22659889161df269ef140f5b024be531
Daniel Worthington-Bodart <dan@bodar.com>
2013-11-15 10:07:20 +0000

    Updated JCompilo version

--- jcompilo.sh
+++ jcompilo.sh
@@ -2,23 +2,26 @@
 
 JAVA_OPTS=-Djava.net.useSystemProxies=true ${JAVA_OPTS}
 BUILD_NUMBER=${BUILD_NUMBER-dev.build}
-version=146
+version=159
 artifact=jcompilo
 group=com/googlecode/${artifact}
 repo=repo.bodar.com
 dir=lib/
 jar=${dir}${artifact}.jar
+pack=${dir}${artifact}.pack.gz
 url=http://${repo}/${group}/${artifact}/${version}/${artifact}-${version}
-remote_jar=${url}.jar
+remote_file=${url}.pack.gz
 remote_sh=${url}.sh
 
 if [ "$1" = "update" ]; then
-	rm ${jar}
+	rm ${jar} ${pack}
 fi
 
 if [ ! -f ${jar} ]; then
-	mkdir -p ${dir} 
-	wget -O ${jar} ${remote_jar} || curl -o ${jar} ${remote_jar}
-	wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
+	mkdir -p ${dir}
+	wget -O ${pack} ${remote_file} || curl -o ${pack} ${remote_file}
+	unpack200 ${pack} ${jar}
+	rm ${pack}
+	#wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
 fi
 exec java -showversion -Dbuild.number=${BUILD_NUMBER} ${JAVA_OPTS} -jar ${jar} $*
