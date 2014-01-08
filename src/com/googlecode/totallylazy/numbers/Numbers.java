d311b732fcf0d3c9ac731af712254b764b1d5160
Daniel Worthington-Bodart <dan@bodar.com>
2014-01-08 13:58:33 +0000

    Removed lambdas and enumerable

--- src/com/googlecode/totallylazy/numbers/Numbers.java
+++ src/com/googlecode/totallylazy/numbers/Numbers.java
@@ -586,8 +586,4 @@ public class Numbers {
     public static Number gcd(Number x, Number y) {
         return integralOperatorsFor(x, y).gcd(x, y);
     }
-
-    public static class parameters {
-        public static Number a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,_;
-    }
 }
