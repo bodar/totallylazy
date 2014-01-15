9ed6fffe8ee1122e7e81bda4942055d662654fbb
Daniel Worthington-Bodart <dan@bodar.com>
2014-01-15 16:14:18 +0000

    Added BiPredicate

--- src/com/googlecode/totallylazy/BinaryPredicate.java
+++ src/com/googlecode/totallylazy/BinaryPredicate.java
@@ -1,5 +1,12 @@
 package com.googlecode.totallylazy;
 
-public interface BinaryPredicate<T> {
+import java.util.function.BiPredicate;
+
+public interface BinaryPredicate<T> extends BiPredicate<T,T> {
     boolean matches(T a, T b);
+
+    @Override
+    default boolean test(T t, T t2) {
+        return matches(t, t2);
+    }
 }
