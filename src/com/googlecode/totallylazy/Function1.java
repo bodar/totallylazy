130f422dd3c0b388d3ef1aa2f79d3cfbfbd14f35
Daniel Worthington-Bodart <dan@bodar.com>
2014-03-28 11:33:00 +0000

    Added Callable1 and Function1 to help migration

---
+++ src/com/googlecode/totallylazy/Function1.java
@@ -0,0 +1,6 @@
+package com.googlecode.totallylazy;
+
+
+/** @deprecated Replaced by {@link com.googlecode.totallylazy.Function} */
+@Deprecated
+public abstract class Function1<A,B> extends Eq implements Function<A,B> {}
