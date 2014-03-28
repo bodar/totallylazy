feded8cd4eed34f7fdd68662db1fd534552a9739
Daniel Worthington-Bodart <dan@bodar.com>
2014-03-28 15:31:01 +0000

    Deprecated Callable4

--- src/com/googlecode/totallylazy/Callable3.java
+++ src/com/googlecode/totallylazy/Callable3.java
@@ -2,5 +2,4 @@ package com.googlecode.totallylazy;
 
 /** @deprecated Replaced by {@link com.googlecode.totallylazy.Function3} */
 @Deprecated
-public interface Callable3<A, B, C, D> {
-}
+public interface Callable3<A, B, C, D> extends Function3<A,B,C,D> { }
