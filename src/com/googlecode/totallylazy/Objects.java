fa27f1320eae77bfb7179f924b9fd785b5e9bbc7
Daniel Worthington-Bodart <dan@bodar.com>
2014-03-28 09:03:01 +0000

    Removed Parameters

--- src/com/googlecode/totallylazy/Objects.java
+++ src/com/googlecode/totallylazy/Objects.java
@@ -19,8 +19,4 @@ public class Objects {
             }
         };
     }
-
-    public static class parameters {
-        public static Object a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,_;
-    }
 }
