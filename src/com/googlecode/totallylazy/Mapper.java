7df7eb68b69a6225d713535b43981c216f22888f
Daniel Worthington-Bodart <dan@bodar.com>
2014-03-28 15:02:14 +0000

    Reverted Trampoline to tail recursive version

--- src/com/googlecode/totallylazy/Mapper.java
+++ src/com/googlecode/totallylazy/Mapper.java
@@ -2,5 +2,4 @@ package com.googlecode.totallylazy;
 
 /** @deprecated Replaced by {@link com.googlecode.totallylazy.Function} */
 @Deprecated
-public abstract class Mapper<T,R> extends Eq implements Function<T,R> {
-}
+public abstract class Mapper<T,R> extends Eq implements Function<T,R> {}
