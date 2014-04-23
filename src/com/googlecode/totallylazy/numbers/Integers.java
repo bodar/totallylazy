65c5cc4e05f9f1eff7b157fc47130bdbad3a4e24
Daniel Worthington-Bodart <dan@bodar.com>
2014-04-23 14:55:32 +0100

    Added Integers.range

--- src/com/googlecode/totallylazy/numbers/Integers.java
+++ src/com/googlecode/totallylazy/numbers/Integers.java
@@ -1,7 +1,10 @@
 package com.googlecode.totallylazy.numbers;
 
+import com.googlecode.totallylazy.Seq;
 import com.googlecode.totallylazy.comparators.Maximum;
 import com.googlecode.totallylazy.comparators.Minimum;
+
+import static com.googlecode.totallylazy.Sequences.iterate;
 
 public class Integers {
     public static Maximum<Integer> maximum() {
@@ -10,5 +13,19 @@ public class Integers {
 
     public static Minimum<Integer> minimum() {
         return Minimum.minimum(Integer.MAX_VALUE);
+    }
+
+    public static Seq<Integer> range(final int start) {
+        return iterate(i -> i + 1, start);
+    }
+
+    public static Seq<Integer> range(final int start, final int end) {
+        if (end < start) return range(start, end, -1);
+        return range(start).takeWhile(i -> i <= end);
+    }
+
+    public static Seq<Integer> range(final int start, final int end, final int step) {
+        if (end < start) return iterate(i -> i + step, start).takeWhile(i -> i >= end);
+        return iterate(i -> i + step, start).takeWhile(i -> i <= end);
     }
 }
