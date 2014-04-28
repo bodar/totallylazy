cb93633812f4dbc3df387861a4e3e2727478d325
Daniel Worthington-Bodart <dan@bodar.com>
2014-04-28 10:28:23 +0100

    Fixed up Integers.range

--- src/com/googlecode/totallylazy/numbers/Integers.java
+++ src/com/googlecode/totallylazy/numbers/Integers.java
@@ -1,10 +1,15 @@
 package com.googlecode.totallylazy.numbers;
 
-import com.googlecode.totallylazy.Seq;
+import com.googlecode.totallylazy.Function1;
+import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.comparators.Maximum;
 import com.googlecode.totallylazy.comparators.Minimum;
+import com.googlecode.totallylazy.predicates.LogicalPredicate;
 
 import static com.googlecode.totallylazy.Sequences.iterate;
+import static com.googlecode.totallylazy.numbers.Integers.functions.greaterThanOrEqualTo;
+import static com.googlecode.totallylazy.numbers.Integers.functions.increment;
+import static com.googlecode.totallylazy.numbers.Integers.functions.lessThanOrEqualTo;
 
 public class Integers {
     public static Maximum.Function<Integer> maximum() {
@@ -15,21 +20,42 @@ public class Integers {
         return Minimum.constructors.minimum(Integer.MAX_VALUE);
     }
 
-    public static class parameters {
-        public static int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,_;
-    }
+    public static Sequence<Integer> range(final int start) { return iterate(increment, start); }
 
-    public static Seq<Integer> range(final int start) {
-        return iterate(i -> i + 1, start);
-    }
-
-    public static Seq<Integer> range(final int start, final int end) {
+    public static Sequence<Integer> range(final int start, final int end) {
         if (end < start) return range(start, end, -1);
-        return range(start).takeWhile(i -> i <= end);
+        return range(start).takeWhile(lessThanOrEqualTo(end));
     }
 
-    public static Seq<Integer> range(final int start, final int end, final int step) {
-        if (end < start) return iterate(i -> i + step, start).takeWhile(i -> i >= end);
-        return iterate(i -> i + step, start).takeWhile(i -> i <= end);
+    public static Sequence<Integer> range(final int start, final int end, final int step) {
+        if (end < start) return iterate(increment, start).takeWhile(greaterThanOrEqualTo(end));
+        return iterate(increment, start).takeWhile(lessThanOrEqualTo(end));
+    }
+
+    public static class functions {
+        public static final Function1<Integer, Integer> increment = new Function1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer i) throws Exception {
+                return i + 1;
+            }
+        };
+
+        public static LogicalPredicate<Integer> lessThanOrEqualTo(final int end) {
+            return new LogicalPredicate<Integer>() {
+                @Override
+                public boolean matches(Integer other) {
+                    return other <= end;
+                }
+            };
+        }
+
+        public static LogicalPredicate<Integer> greaterThanOrEqualTo(final int end) {
+            return new LogicalPredicate<Integer>() {
+                @Override
+                public boolean matches(Integer other) {
+                    return other >= end;
+                }
+            };
+        }
     }
 }
