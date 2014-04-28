8369f39fa0a3d7327378894a643556d0c11b4883
Daniel Worthington-Bodart <dan@bodar.com>
2014-04-28 10:32:05 +0100

    Fixed tests

--- src/com/googlecode/totallylazy/numbers/Integers.java
+++ src/com/googlecode/totallylazy/numbers/Integers.java
@@ -7,6 +7,7 @@ import com.googlecode.totallylazy.comparators.Minimum;
 import com.googlecode.totallylazy.predicates.LogicalPredicate;
 
 import static com.googlecode.totallylazy.Sequences.iterate;
+import static com.googlecode.totallylazy.numbers.Integers.functions.add;
 import static com.googlecode.totallylazy.numbers.Integers.functions.greaterThanOrEqualTo;
 import static com.googlecode.totallylazy.numbers.Integers.functions.increment;
 import static com.googlecode.totallylazy.numbers.Integers.functions.lessThanOrEqualTo;
@@ -20,7 +21,9 @@ public class Integers {
         return Minimum.constructors.minimum(Integer.MAX_VALUE);
     }
 
-    public static Sequence<Integer> range(final int start) { return iterate(increment, start); }
+    public static Sequence<Integer> range(final int start) {
+        return iterate(increment, start);
+    }
 
     public static Sequence<Integer> range(final int start, final int end) {
         if (end < start) return range(start, end, -1);
@@ -28,17 +31,21 @@ public class Integers {
     }
 
     public static Sequence<Integer> range(final int start, final int end, final int step) {
-        if (end < start) return iterate(increment, start).takeWhile(greaterThanOrEqualTo(end));
-        return iterate(increment, start).takeWhile(lessThanOrEqualTo(end));
+        if (end < start) return iterate(add(step), start).takeWhile(greaterThanOrEqualTo(end));
+        return iterate(add(step), start).takeWhile(lessThanOrEqualTo(end));
     }
 
     public static class functions {
-        public static final Function1<Integer, Integer> increment = new Function1<Integer, Integer>() {
-            @Override
-            public Integer call(Integer i) throws Exception {
-                return i + 1;
-            }
-        };
+        public static final Function1<Integer, Integer> increment = add(1);
+
+        public static Function1<Integer, Integer> add(final int value) {
+            return new Function1<Integer, Integer>() {
+                @Override
+                public Integer call(Integer i) throws Exception {
+                    return i + value;
+                }
+            };
+        }
 
         public static LogicalPredicate<Integer> lessThanOrEqualTo(final int end) {
             return new LogicalPredicate<Integer>() {
