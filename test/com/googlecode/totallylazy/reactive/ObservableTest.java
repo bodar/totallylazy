23c35815c52e69dd0990dab073422b0116fca8a3
Daniel Worthington-Bodart <dan@bodar.com>
2014-06-18 15:33:43 +0100

    Cleanup test

--- test/com/googlecode/totallylazy/reactive/ObservableTest.java
+++ test/com/googlecode/totallylazy/reactive/ObservableTest.java
@@ -13,84 +13,69 @@ import static com.googlecode.totallylazy.reactive.Observable.observable;
 public class ObservableTest {
     @Test
     public void canObserverItems() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3).subscribe(observer);
-        assertObserved(observer, 1, 2, 3);
+        assertObserved(observable(1, 2, 3), 1, 2, 3);
     }
 
     @Test
     public void supportsFiltering() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3).filter(i -> i % 2 == 0).subscribe(observer);
-        assertObserved(observer, 2);
+        assertObserved(observable(1, 2, 3).filter(i -> i % 2 == 0), 2);
     }
 
     @Test
     public void supportsMapping() throws Exception {
-        CapturingObserver<String> observer = new CapturingObserver<>();
-        observable(1, 2, 3).map(Object::toString).subscribe(observer);
-        assertObserved(observer, "1", "2", "3");
+        assertObserved(observable(1, 2, 3).map(Object::toString), "1", "2", "3");
     }
 
     @Test
     public void supportsFlatMapping() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3).flatMap(i -> observable(i, i * 2)).subscribe(observer);
-        assertObserved(observer, 1, 2, 2, 4, 3, 6);
+        assertObserved(observable(1, 2, 3).flatMap((Integer i) -> observable(i, i * 2)), 1, 2, 2, 4, 3, 6);
     }
 
     @Test
     public void supportsScan() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(0, 2, 4).scan(average).map(Number::intValue).subscribe(observer);
-        assertObserved(observer, 0, 1, 2);
+        assertObserved(observable(0, 2, 4).scan(average).map(Number::intValue), 0, 1, 2);
     }
 
     @Test
     public void supportsLast() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(0, 2, 4).last().subscribe(observer);
-        assertObserved(observer, 4);
+        assertObserved(observable(0, 2, 4).last(), 4);
     }
 
     @Test
     public void supportsReduce() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(0, 2, 4).reduce(average).map(Number::intValue).subscribe(observer);
-        assertObserved(observer, 2);
+        assertObserved(observable(0, 2, 4).reduce(average).map(Number::intValue),
+                2);
     }
 
     @Test
     public void supportsTake() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3, 4, 5, 6).take(3).subscribe(observer);
-        assertObserved(observer, 1, 2, 3);
+        assertObserved(observable(1, 2, 3, 4, 5, 6).take(3),
+                1, 2, 3);
     }
 
     @Test
     public void supportsTakeWhile() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3, 4, 5, 6).takeWhile(i -> i < 4).subscribe(observer);
-        assertObserved(observer, 1, 2, 3);
+        assertObserved(observable(1, 2, 3, 4, 5, 6).takeWhile(i -> i < 4),
+                1, 2, 3);
     }
 
     @Test
     public void supportsDrop() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3, 4, 5, 6).drop(3).subscribe(observer);
-        assertObserved(observer, 4, 5, 6);
+        assertObserved(observable(1, 2, 3, 4, 5, 6).drop(3),
+                4, 5, 6);
     }
 
     @Test
     public void supportsDropWhile() throws Exception {
-        CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3, 4, 5, 6).dropWhile(i -> i < 4).subscribe(observer);
-        assertObserved(observer, 4, 5, 6);
+        assertObserved(observable(1, 2, 3, 4, 5, 6).dropWhile(i -> i < 4),
+                4, 5, 6);
     }
 
 
     @SafeVarargs
-    private final <T> void assertObserved(CapturingObserver<T> observer, T... values) {
+    private final <T> void assertObserved(Observable<T> observable, T... values) {
+        CapturingObserver<T> observer = new CapturingObserver<>();
+        observable.subscribe(observer);
         assertThat(observer.items(), hasExactly(values));
         assertThat(observer.error(), nullValue());
         assertThat(observer.completed(), is(true));
