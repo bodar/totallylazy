ed3bd83df589883146847a717093e5e2f855b527
Daniel Worthington-Bodart <dan@bodar.com>
2014-06-18 14:04:51 +0100

    Added scan support to Observable

--- test/com/googlecode/totallylazy/reactive/ObservableTest.java
+++ test/com/googlecode/totallylazy/reactive/ObservableTest.java
@@ -1,11 +1,13 @@
 package com.googlecode.totallylazy.reactive;
 
+import com.googlecode.totallylazy.numbers.Numbers;
 import org.junit.Test;
 
 import static com.googlecode.totallylazy.Assert.assertThat;
 import static com.googlecode.totallylazy.Predicates.is;
 import static com.googlecode.totallylazy.Predicates.nullValue;
 import static com.googlecode.totallylazy.matchers.IterablePredicates.hasExactly;
+import static com.googlecode.totallylazy.numbers.Numbers.average;
 import static com.googlecode.totallylazy.reactive.Observable.observable;
 
 public class ObservableTest {
@@ -33,11 +35,19 @@ public class ObservableTest {
     @Test
     public void supportsFlatMapping() throws Exception {
         CapturingObserver<Integer> observer = new CapturingObserver<>();
-        observable(1, 2, 3).flatMap(i -> observable(i, 0)).subscribe(observer);
-        assertObserved(observer, 1, 0, 2, 0, 3, 0);
+        observable(1, 2, 3).flatMap(i -> observable(i, i * 2)).subscribe(observer);
+        assertObserved(observer, 1, 2, 2, 4, 3, 6);
     }
 
-    private <T> void assertObserved(CapturingObserver<T> observer, T... values) {
+    @Test
+    public void supportsScan() throws Exception {
+        CapturingObserver<Integer> observer = new CapturingObserver<>();
+        observable(0, 2, 4).scan(average).map(Number::intValue).subscribe(observer);
+        assertObserved(observer, 0, 1, 2);
+    }
+
+    @SafeVarargs
+    private final <T> void assertObserved(CapturingObserver<T> observer, T... values) {
         assertThat(observer.items(), hasExactly(values));
         assertThat(observer.error(), nullValue());
         assertThat(observer.completed(), is(true));
