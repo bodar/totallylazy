1bbb13535761be4ab0588ba94fcfabe7a02ef688
Daniel Worthington-Bodart <dan@bodar.com>
2014-06-20 12:11:53 +0100

    Updated jcompilo

--- jcompilo.sh
+++ jcompilo.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
+set -e
 
-if [ -n "${JAVA_HOME}" ]; then
-	PATH=${JAVA_HOME}/bin:${PATH}
-fi
-
+JAVA_VERSION=8
 JAVA_OPTS="-Djava.net.useSystemProxies=true ${JAVA_OPTS}"
 BUILD_NUMBER=${BUILD_NUMBER-dev.build}
-version=176
+version=180
 artifact=jcompilo
 group=com/googlecode/${artifact}
 repo=repo.bodar.com
@@ -17,6 +15,8 @@ url=http://${repo}/${group}/${artifact}/${version}/${artifact}-${version}
 remote_file=${url}.pack.gz
 remote_sh=${url}.sh
 
+type -t setjava > /dev/null && setjava -q ${JAVA_VERSION} || if [ -n "${JAVA_HOME}" ]; then PATH=${JAVA_HOME}/bin:${PATH}; fi
+
 if [ "$1" = "update" ]; then
 	rm ${jar} ${pack}
 fi
@@ -26,6 +26,6 @@ if [ ! -f ${jar} ]; then
 	wget -O ${pack} ${remote_file} || curl -o ${pack} ${remote_file}
 	unpack200 ${pack} ${jar}
 	rm ${pack}
-	#wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
+	wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
 fi
 exec java -showversion -Dbuild.number=${BUILD_NUMBER} ${JAVA_OPTS} -jar ${jar} $*
