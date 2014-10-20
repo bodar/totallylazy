b196c0819f8c2390f7eb99eb4155124e3762096d
Daniel Worthington-Bodart <dan.bodart@springer.com>
2014-10-20 16:39:34 +0100

    Updated JCompilo to get commit info from git

--- jcompilo.sh
+++ jcompilo.sh
@@ -4,7 +4,7 @@ set -e
 JAVA_VERSION=${JAVA_VERSION-7}
 JAVA_OPTS="-server -XX:+TieredCompilation -Djava.net.useSystemProxies=true ${JAVA_OPTS}"
 BUILD_NUMBER=${BUILD_NUMBER-dev.build}
-version=180
+version=194
 artifact=jcompilo
 group=com/googlecode/${artifact}
 repo=repo.bodar.com
