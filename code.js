4e0f40bfc5f7355bbb434ed0f1cfe1b1405b59b8
Daniel Worthington-Bodart <dan.bodart@springer.com>
2014-10-23 12:30:18 +0100

    Removed jQuery

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+Array.prototype.each = Array.prototype.forEach;
+
 RegExp.prototype.replace = function(str, replacer, nonMatchedReplacer) {
     nonMatchedReplacer = nonMatchedReplacer || function(value) {
         return value
@@ -16,43 +18,53 @@ RegExp.prototype.replace = function(str, replacer, nonMatchedReplacer) {
     return result.join("");
 };
 
-function yatspec() {}
+function select(selector, element){
+    element = element || document;
+    var nodes = element.querySelectorAll(selector);
+    var array = [];
+    for (var i = 0; i < nodes.length; ++i) {
+        array[i] = nodes[i];
+    }
+    return array;
+}
 
-yatspec.highlight = function(element, pairs) {
+function code() {}
+
+code.highlight = function(element, pairs) {
     if (pairs.length == 0) {
         return;
     }
     var classes = [];
     var matchGroups = [];
-    $.each(pairs, function() {
-        matchGroups.push("(", this.pattern, ")", "|");
-        classes.push(this.cssClass);
+    pairs.each(function(p) {
+        matchGroups.push("(", p.pattern, ")", "|");
+        classes.push(p.cssClass);
     });
     matchGroups.pop();
     var regex = new RegExp(matchGroups.join(""), "gm");
 
-    $(element).html(regex.replace($(element).html(), function(match) {
+    element.innerHTML = regex.replace(element.innerHTML, function(match) {
         var matches = match.slice(1);
         for (var i = 0; i < matches.length; i++) {
             if (matches[i]) {
                 return '<span class="' + classes[i] + '">' + matches[i] + '</span>'
             }
         }
-    }));
+    });
 };
 
-$(document).ready(function () {
-    $('code.language-xml', this).each(function() {
-        yatspec.highlight(this, [
+document.addEventListener('DOMContentLoaded', function () {
+    select('code.language-xml').each(function(element) {
+        code.highlight(element, [
             {pattern: '"[^"]*"', cssClass: "quote" },
             {pattern: "&lt;[^\\s&]+", cssClass: "keyword" },
             {pattern: "\\??&gt;", cssClass: "keyword" },
             {pattern: "\\s[\\w:-]+=", cssClass: "constant" }
         ]);
-    })
+    });
 
-    $('code.language-java', this).each(function() {
-        yatspec.highlight(this, [
+    select('code.language-java').each(function(element) {
+        code.highlight(element, [
             {pattern: '"[^"]*"', cssClass: "quote" },
             {pattern: "(?:public|void|class|true|false|throws|[,.;])", cssClass: "keyword" },
             {pattern: "\\d+", cssClass: "literal" },
@@ -60,4 +72,7 @@ $(document).ready(function () {
             {pattern: "\\w+\\b(?!\\()", cssClass: "keyword" },
         ]);
     })
-}, false);
+});
+
+
+
