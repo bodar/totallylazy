8c8e3f245f6df2111c11a1c7d6981d39bf49bce4
Daniel Worthington-Bodart <dan@bodar.com>
2014-11-12 21:49:30 +0000

    Moved trampoline method internally

--- src/com/googlecode/totallylazy/Trampoline.java
+++ src/com/googlecode/totallylazy/Trampoline.java
@@ -9,12 +9,6 @@ public interface Trampoline<T> extends Returns<T> {
 
     default Trampoline<T> next() { throw new NoSuchElementException(); }
 
-    @tailrec
-    static <T> T trampoline(Trampoline<T> trampoline) {
-        if(trampoline.done()) return trampoline.apply();
-        return trampoline(trampoline.next());
-    }
-
     static <T> Trampoline<T> done(T value) { return () -> value; }
 
     static <T> Trampoline<T> more(Returns<? extends Trampoline<T>> next) {
@@ -24,6 +18,12 @@ public interface Trampoline<T> extends Returns<T> {
             @Override public T call() throws Exception { return trampoline(this); }
 
             @Override public Trampoline<T> next() { return next.apply(); }
+
+            @tailrec
+            T trampoline(Trampoline<T> trampoline) throws Exception {
+                if(trampoline.done()) return trampoline.call();
+                return trampoline(trampoline.next());
+            }
         };
     }
 }
