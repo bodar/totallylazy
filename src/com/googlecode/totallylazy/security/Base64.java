f28c2d3bd6ff50fa1e546f2f16367e3cb4514fe9
Daniel Worthington-Bodart <dan@bodar.com>
2015-05-22 15:38:24 +0100

    Removed dependency on Apache codec

--- src/com/googlecode/totallylazy/security/Base64.java
+++ src/com/googlecode/totallylazy/security/Base64.java
@@ -1,15 +1,11 @@
 package com.googlecode.totallylazy.security;
 
-import com.googlecode.totallylazy.Strings;
-
-import static com.googlecode.totallylazy.Strings.bytes;
-
 public class Base64 {
     public static byte[] decode(String content) {
-        return new org.apache.commons.codec.binary.Base64().decode(bytes(content));
+        return javax.xml.bind.DatatypeConverter.parseBase64Binary(content);
     }
 
     public static String encode(byte[] content) {
-        return Strings.toString(new org.apache.commons.codec.binary.Base64().encode(content));
+        return javax.xml.bind.DatatypeConverter.printBase64Binary(content);
     }
 }
---
+++ test/com/googlecode/totallylazy/security/Base64Test.java
@@ -0,0 +1,15 @@
+package com.googlecode.totallylazy.security;
+
+import org.junit.Test;
+
+import static com.googlecode.totallylazy.Strings.bytes;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Base64Test {
+    @Test
+    public void doesNotLineWrap() throws Exception {
+        String encoded = Base64.encode(bytes("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+        assertThat(encoded, is("TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0IGVuaW0gYWQgbWluaW0gdmVuaWFtLCBxdWlzIG5vc3RydWQgZXhlcmNpdGF0aW9uIHVsbGFtY28gbGFib3JpcyBuaXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kbyBjb25zZXF1YXQuIER1aXMgYXV0ZSBpcnVyZSBkb2xvciBpbiByZXByZWhlbmRlcml0IGluIHZvbHVwdGF0ZSB2ZWxpdCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVnaWF0IG51bGxhIHBhcmlhdHVyLiBFeGNlcHRldXIgc2ludCBvY2NhZWNhdCBjdXBpZGF0YXQgbm9uIHByb2lkZW50LCBzdW50IGluIGN1bHBhIHF1aSBvZmZpY2lhIGRlc2VydW50IG1vbGxpdCBhbmltIGlkIGVzdCBsYWJvcnVtLg=="));
+    }
+}
