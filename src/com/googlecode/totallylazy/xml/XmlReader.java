971a0a5632794f51c97f086c4f1584fcbad163c1
Daniel Worthington-Bodart <dan@bodar.com>
2015-05-29 13:11:09 +0100

    Switched around order

--- src/com/googlecode/totallylazy/xml/XmlReader.java
+++ src/com/googlecode/totallylazy/xml/XmlReader.java
@@ -1,7 +1,10 @@
 package com.googlecode.totallylazy.xml;
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Callable2;
 import com.googlecode.totallylazy.LazyException;
 import com.googlecode.totallylazy.Predicate;
+import com.googlecode.totallylazy.Rules;
 import com.googlecode.totallylazy.Unchecked;
 import com.googlecode.totallylazy.Xml;
 import com.googlecode.totallylazy.iterators.StatefulIterator;
@@ -24,31 +27,37 @@ import static com.googlecode.totallylazy.Sequences.forwardOnly;
 import static com.googlecode.totallylazy.xml.StreamingXPath.descendant;
 import static com.googlecode.totallylazy.xml.StreamingXPath.name;
 
-public class XmlReader extends StatefulIterator<Node> {
+public class XmlReader<T> extends StatefulIterator<T> {
     private final XMLEventReader reader;
     private final Predicate<Location> predicate;
+    private final Callable2<Location, XMLEventReader, T> function;
     private final Location path = new Location();
 
-    private XmlReader(XMLEventReader reader, Predicate<Location> predicate) {
+    private XmlReader(XMLEventReader reader, Predicate<Location> predicate, Callable2<Location, XMLEventReader, T> function) {
         this.reader = reader;
         this.predicate = predicate;
+        this.function = function;
     }
 
-    public static XmlReader xmlReader(Reader reader, String localName) throws LazyException {
+    public static XmlReader<Node> xmlReader(Reader reader, String localName) throws LazyException {
         return xmlReader(reader, descendant(name(localName)));
     }
 
-    public static XmlReader xmlReader(Reader reader, Predicate<Location> predicate) {
+    public static XmlReader<Node> xmlReader(Reader reader, Predicate<Location> predicate) {
+            return xmlReader(reader, predicate, (location, xmlEventReader) -> new NodeCreator(xmlEventReader).call(location));
+    }
+
+    public static <T> XmlReader<T> xmlReader(Reader reader, Predicate<Location> predicate, Callable2<Location, XMLEventReader, T> function) {
         try {
             XMLInputFactory factory = XMLInputFactory.newInstance();
-            return new XmlReader(factory.createXMLEventReader(reader), predicate);
+            return new XmlReader<>(factory.createXMLEventReader(reader), predicate, function);
         } catch (XMLStreamException e) {
             throw lazyException(e);
         }
     }
 
     @Override
-    protected Node getNext() throws Exception {
+    protected T getNext() throws Exception {
         while (reader.hasNext()) {
             XMLEvent event = reader.nextEvent();
             if (event instanceof EndElement) path.remove();
@@ -56,37 +65,53 @@ public class XmlReader extends StatefulIterator<Node> {
                 StartElement start = (StartElement) event;
                 path.add(start);
                 if (predicate.matches(path)) {
+                    T t = function.call(path, reader);
                     path.remove();
-                    return children(copyAttributes(start, element(start.getName().getLocalPart())));
+                    return t;
                 }
             }
         }
         return finished();
     }
 
-    private Element element(String name) {
-        return Xml.document("<" + name + "/>").getDocumentElement();
-    }
+    private static class NodeCreator implements Callable1<Location, Node> {
+        private final XMLEventReader reader;
 
-    private Node children(Node parent) throws XMLStreamException {
-        while (reader.hasNext()) {
-            XMLEvent event = reader.nextEvent();
-            if (event instanceof EndElement) return parent;
-            if (event instanceof Characters) parent.appendChild(parent.getOwnerDocument().createTextNode(((Characters) event).getData()));
-            if (event instanceof StartElement) children(child(parent, (StartElement) event));
+        public NodeCreator(XMLEventReader reader) {
+            this.reader = reader;
         }
-        return parent;
-    }
 
-    private Node child(Node parent, StartElement start) {
-        return parent.appendChild(copyAttributes(start, parent.getOwnerDocument().createElement(start.getName().getLocalPart())));
-    }
-
-    private Element copyAttributes(StartElement source, Element destination) {
-        for (Attribute attribute : forwardOnly(Unchecked.<Iterator<Attribute>>cast(source.getAttributes()))) {
-            destination.setAttribute(attribute.getName().getLocalPart(), attribute.getValue());
+        @Override
+        public Node call(Location location) throws Exception {
+            StartElement start = location.current();
+            return children(copyAttributes(start, element(start.getName().getLocalPart())));
         }
-        return destination;
-    }
 
+        private Element element(String name) {
+            return Xml.document("<" + name + "/>").getDocumentElement();
+        }
+
+        private Node children(Node parent) throws XMLStreamException {
+            while (reader.hasNext()) {
+                XMLEvent event = reader.nextEvent();
+                if (event instanceof EndElement) return parent;
+                if (event instanceof Characters) parent.appendChild(parent.getOwnerDocument().createTextNode(((Characters) event).getData()));
+                if (event instanceof StartElement) children(child(parent, (StartElement) event));
+            }
+            return parent;
+        }
+
+        private Node child(Node parent, StartElement start) {
+            return parent.appendChild(copyAttributes(start, parent.getOwnerDocument().createElement(start.getName().getLocalPart())));
+        }
+
+        private Element copyAttributes(StartElement source, Element destination) {
+            for (Attribute attribute : forwardOnly(Unchecked.<Iterator<Attribute>>cast(source.getAttributes()))) {
+                destination.setAttribute(attribute.getName().getLocalPart(), attribute.getValue());
+            }
+            return destination;
+        }
+
+
+    }
 }
