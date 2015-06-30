17b01b221fa28732f6246c31ed0cd816188dadb2
Daniel Worthington-Bodart <dan@bodar.com>
2015-06-30 06:35:18 +0100

    Removed unused class

--- src/com/googlecode/totallylazy/proxy/DefinableClassLoader.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.totallylazy.proxy;
-
-public class DefinableClassLoader extends ClassLoader {
-    public Class<?> defineClass(String name, byte[] bytes) {
-        return defineClass(name, bytes, 0, bytes.length);
-    }
-}
