2749210416053d64c7f2852717e6f3786c1bbbfb
Jamie Shiell <jamie@infernus.org>
2022-02-22 10:18:46 +0000

    Backport move to GH release process due to Bintray shutdown

--- jcompilo.sh
+++ jcompilo.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 set -e
 
-JAVA_VERSION=${JAVA_VERSION-7}
+
+JAVA_VERSION=${JAVA_VERSION-8}
 JAVA_OPTS="-server -XX:+TieredCompilation -Djava.net.useSystemProxies=true ${JAVA_OPTS}"
 BUILD_NUMBER=${BUILD_NUMBER-dev.build}
-version=1.10
+version=1.11
 artifact=jcompilo
-group=com/googlecode/${artifact}
-repo=dl.bintray.com/bodar/maven
 dir=lib/
 jar=${dir}${artifact}.jar
-pack=${dir}${artifact}.pack.gz
-url=http://${repo}/${group}/${artifact}/${version}/${artifact}-${version}
-remote_file=${url}.pack.gz
+url=https://github.com/bodar/${artifact}/releases/download/${version}/${artifact}-${version}.jar
 remote_sh=${url}.sh
 
 type -t setjava > /dev/null && setjava -q ${JAVA_VERSION} || if [ -n "${JAVA_HOME}" ]; then PATH=${JAVA_HOME}/bin:${PATH}; fi
 
 if [ "$1" = "update" ]; then
-	rm -f ${jar} ${pack}
+	rm -f ${jar}
 fi
 
 if [ ! -f ${jar} ]; then
 	mkdir -p ${dir}
-	wget -O ${pack} ${remote_file} || curl -o ${pack} ${remote_file}
-	unpack200 ${pack} ${jar}
-	rm -f ${pack}
-	#wget -O $0 ${remote_sh} || curl -o $0 ${remote_sh}
+	wget -O ${jar} ${url} || curl -o ${jar} ${url}
 fi
 exec java -showversion -Dbuild.number=${BUILD_NUMBER} ${JAVA_OPTS} -jar ${jar} $*
